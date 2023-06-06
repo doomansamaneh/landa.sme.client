b4c76c418829541ef952e985d45025ab9726465c
محمد ینگی ملکی <yengi@gamma.local>
2023-06-06 12:45:18 +0330

    screen mode desktop and mobile

--- src/layouts/BusinessLayout.vue
+++ src/layouts/BusinessLayout.vue
@@ -1,49 +1,5 @@
 <template>
   <q-layout view="hHh lpR fFf">
-    <!-- <q-header elevated class="bg-teal text-white shadow-2">
-      <q-toolbar>
-        <q-avatar color="white" class="q-mx-sm" text-color="black">A</q-avatar>
-        <q-separator dark vertical />
-
-        <q-btn-dropdown stretch flat no-caps dropdown-icon="keyboard_arrow_down">
-          <template v-slot:label>
-            <div class="row items-center no-wrap">
-              <div>
-                {{username}}
-              </div>
-            </div>
-          </template>
-
-          <q-list dense padding style="min-width: 240px">
-            <q-item-label class="text-h5" header>
-              <q-avatar color="blue-4" class="q-mx-sm" text-color="white">A</q-avatar>
-              {{username}}
-            </q-item-label>
-            <q-item clickable v-close-popup>
-              <q-item-section avatar>
-                <q-avatar icon="password" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>change password</q-item-label>
-              </q-item-section>
-            </q-item>
-
-            <q-item clickable v-close-popup tabindex="0" @click="authStore.logout()">
-              <q-item-section avatar>
-                <q-avatar icon="logout" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>logout</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-btn-dropdown>
-
-        <q-space />
-        <div>Landa SME v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header> -->
-
     <q-header class="bg-white text-black q-qx-md">
       <q-toolbar class="q-px-lg">
         <q-toolbar-title class="text-black"
--- src/pages/business/IndexPage.vue
+++ src/pages/business/IndexPage.vue
@@ -1,86 +1,85 @@
 <template>
-  <!-- <q-page class="row justify-center items-center bg-blue-1"> -->
-     <!-- <div class="row q-py-md">
-      <div class="col col-3"></div>
-      <div class="col">
-        <div class="q-pb-md q-gutter-sm">
-          <q-btn color="blue" @click="reloadData()" no-caps icon="refresh" label="load data" />
-          <q-btn color="red" @click="gotoBusiness()" no-caps icon="directions" label="Goto Business" />
-        </div>
-        <div>
-          <data-grid ref="gridBusiness" title="my business list" color="bg-primary text-white"
-            dataSource="business/getBusinessGridData" :columns="columns" @changeSelected="gridChangeSelected">
-          </data-grid>
-        </div>
+  <!-- <div class="row q-py-md">
+    <div class="col col-3"></div>
+    <div class="col">
+      <div class="q-pb-md q-gutter-sm">
+        <q-btn color="blue" @click="reloadData()" no-caps icon="refresh" label="load data" />
+        <q-btn color="red" @click="gotoBusiness()" no-caps icon="directions" label="Goto Business" />
       </div>
-      <div class="col col-3"></div>
-    </div> -->
+      <div>
+        <data-grid ref="gridBusiness" title="my business list" color="bg-primary text-white"
+          dataSource="business/getBusinessGridData" :columns="columns" @changeSelected="gridChangeSelected">
+        </data-grid>
+      </div>
+    </div>
+    <div class="col col-3"></div>
+  </div> -->
 
   <q-page class="row justify-center items-center bg-blue-1">
     <MobileView />
-    <DesktopView />
+    <DesktopView/>
   </q-page>
 </template>
 
 <script setup>
-import { ref } from "vue"
-import DataGrid from "../../components/shared/DataGrid.vue"
-import MobileView from "src/components/business/MobileView.vue"
-import DesktopView from "src/components/business/DesktopView.vue"
+  import { ref } from "vue"
+  import DataGrid from "../../components/shared/DataGrid.vue"
+  import MobileView from "src/components/business/MobileView.vue"
+  import DesktopView from "src/components/business/DesktopView.vue"
 
-import { fetchWrapper } from "../../helpers"
+  import { fetchWrapper } from "../../helpers"
 
-const gridBusiness = ref(null)
+  const gridBusiness = ref(null)
 
-const columns = [
-  {
-    name: "row-number",
-    required: true,
-    label: "#",
-    align: "left",
-    sortable: false,
-    headerStyle: "width: 5px"
-  },
-  {
-    name: "name",
-    required: true,
-    label: "name",
-    align: "left",
-    field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true
-  },
-  {
-    name: "toDate",
-    required: true,
-    label: "to date",
-    align: "left",
-    field: "toDateString",
-    sortable: true,
-    headerStyle: "width: 50px"
-  },
-  {
-    name: "dateCreated",
-    required: true,
-    label: "dayes to expire",
-    field: (row) => row.daysToExpire,
-    sortable: true,
-    headerStyle: "width: 50px"
+  const columns = [
+    {
+      name: "row-number",
+      required: true,
+      label: "#",
+      align: "left",
+      sortable: false,
+      headerStyle: "width: 5px"
+    },
+    {
+      name: "name",
+      required: true,
+      label: "name",
+      align: "left",
+      field: (row) => row.name,
+      format: (val) => `${val}`,
+      sortable: true
+    },
+    {
+      name: "toDate",
+      required: true,
+      label: "to date",
+      align: "left",
+      field: "toDateString",
+      sortable: true,
+      headerStyle: "width: 50px"
+    },
+    {
+      name: "dateCreated",
+      required: true,
+      label: "dayes to expire",
+      field: (row) => row.daysToExpire,
+      sortable: true,
+      headerStyle: "width: 50px"
+    }
+  ]
+
+  function reloadData() {
+    gridBusiness.value.reloadData()
   }
-]
 
-function reloadData() {
-  gridBusiness.value.reloadData()
-}
-
-async function gotoBusiness() {
-  await fetchWrapper
-    .post("business/gotoBusiness/41f1c444-8450-40c9-b590-c0807a41a4e5")
-    .then((response) => {
-      console.log(response)
-    })
-    .finally(() => {})
-}
+  async function gotoBusiness() {
+    await fetchWrapper
+      .post("business/gotoBusiness/41f1c444-8450-40c9-b590-c0807a41a4e5")
+      .then((response) => {
+        console.log(response)
+      })
+      .finally(() => { })
+  }
 
 </script>
 
