d2b5aed03642151c6aaeedf0436d9e9d9fd3d364
Mohammad Yengimolki <gammadesk@gmail.com>
2023-06-22 09:17:34 +0330

    merge

--- src/pages/business/IndexPage.vue
+++ src/pages/business/IndexPage.vue
@@ -56,14 +56,8 @@ function reloadData() {
   gridBusiness.value.reloadData()
 }
 
-async function gotoBusiness() {
-  await fetchWrapper
-    .post("business/gotoBusiness/41f1c444-8450-40c9-b590-c0807a41a4e5")
-    .then((response) => {
-      console.log(response)
-    })
-    .finally(() => {})
-}
 </script>
 
-<style></style>
+<style>
+  
+</style>
