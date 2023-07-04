bd4b4c7c3b64348914025c2fa0f4b7d837b3609a
Mohammad Yengimolki <gammadesk@gmail.com>
2023-07-04 12:25:07 +0330

    sync with khashayar

---
+++ src/components/shared/TestContainer.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <slot></slot>
+        <div class="header">
+            <h2>this is header slot</h2>
+            <slot name="header"></slot>
+        </div>
+        <div class="body">
+            <slot name="body"></slot>
+        </div>
+        <div class="footer">
+            <slot class="footer" name="footer"></slot>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+    .header {
+        background-color: #dddd;
+        padding: 15px;
+    }
+
+    .footer {
+        background-color: #e5e5e5;
+        padding: 10px;
+        border: 1px solid #dddd;
+    }
+</style>
