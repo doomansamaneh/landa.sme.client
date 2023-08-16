d5b9127a055d67caa8f47b768ca55095a85f0a75
Khashayar Shomali <khashi792017@gmail.com>
2023-08-16 19:44:57 +0330

    Update Dictionary.

--- src/components/users/ChangePassword.vue
+++ src/components/users/ChangePassword.vue
@@ -12,96 +12,134 @@
 
     <q-card-section>
       <q-form ref="form" autofocus>
-        <q-input outlined v-model="oldPassword" :type="isPwdOldPassword ? 'password' : 'text'"
-          :placeholder="$t('change-password-page.placeholders.old-password')" dense class="text-body" required
-          lazy-rules :rules="[(val) => val !== null && val !== '']">
+        <q-input
+          outlined
+          v-model="oldPassword"
+          :type="isPwdOldPassword ? 'password' : 'text'"
+          :placeholder="$t('change-password-page.placeholders.old-password')"
+          dense
+          class="text-body"
+          required
+          lazy-rules
+          :rules="[(val) => val !== null && val !== '']"
+        >
           <template v-slot:append>
-            <q-icon :name="isPwdOldPassword ? 'visibility_off' : 'visibility'" size="xs" class="cursor-pointer"
-              @click="isPwdOldPassword = !isPwdOldPassword" />
+            <q-icon
+              :name="isPwdOldPassword ? 'visibility_off' : 'visibility'"
+              size="xs"
+              class="cursor-pointer"
+              @click="isPwdOldPassword = !isPwdOldPassword"
+            />
           </template>
         </q-input>
 
-        <q-input outlined v-model="newPassword" :type="isPwdNewPassword ? 'password' : 'text'"
-          :placeholder="$t('change-password-page.placeholders.new-password')" dense class="text-body" required
-          lazy-rules :rules="[(val) => val !== null && val !== '']">
+        <q-input
+          outlined
+          v-model="newPassword"
+          :type="isPwdNewPassword ? 'password' : 'text'"
+          :placeholder="$t('change-password-page.placeholders.new-password')"
+          dense
+          class="text-body"
+          required
+          lazy-rules
+          :rules="[(val) => val !== null && val !== '']"
+        >
           <template v-slot:append>
-            <q-icon :name="isPwdNewPassword ? 'visibility_off' : 'visibility'" size="xs" class="cursor-pointer"
-              @click="isPwdNewPassword = !isPwdNewPassword" />
+            <q-icon
+              :name="isPwdNewPassword ? 'visibility_off' : 'visibility'"
+              size="xs"
+              class="cursor-pointer"
+              @click="isPwdNewPassword = !isPwdNewPassword"
+            />
           </template>
         </q-input>
 
-        <q-input outlined v-model="confirmNewPassword" :type="isPwdConfirmPassword ? 'password' : 'text'" :placeholder="
+        <q-input
+          outlined
+          v-model="confirmNewPassword"
+          :type="isPwdConfirmPassword ? 'password' : 'text'"
+          :placeholder="
             $t('change-password-page.placeholders.confirm-password')
-          " dense class="text-body" required lazy-rules :rules="[(val) => val !== null && val !== '']">
+          "
+          dense
+          class="text-body"
+          required
+          lazy-rules
+          :rules="[(val) => val !== null && val !== '']"
+        >
           <template v-slot:append>
-            <q-icon :name="isPwdConfirmPassword ? 'visibility_off' : 'visibility'" size="xs" class="cursor-pointer"
-              @click="isPwdConfirmPassword = !isPwdConfirmPassword" />
+            <q-icon
+              :name="isPwdConfirmPassword ? 'visibility_off' : 'visibility'"
+              size="xs"
+              class="cursor-pointer"
+              @click="isPwdConfirmPassword = !isPwdConfirmPassword"
+            />
           </template>
         </q-input>
       </q-form>
     </q-card-section>
 
     <actions @ok-clicked="submitForm">
-      <template #ok-label>{{ $t('change-password-page.buttons.change-password') }}</template>
+      <template #ok-label>{{
+        $t("change-password-page.buttons.change-password")
+      }}</template>
     </actions>
   </q-card>
 </template>
 
 <script setup>
-  import { ref } from "vue"
+import { ref } from "vue"
 
-  import { useAuthStore } from "src/stores"
-  import { fetchWrapper } from "src/helpers"
-  import BackButton from "src/components/shared/buttons/BackButton.vue"
-  import Actions from "src/components/shared/forms/FormCardActions.vue"
+import { useAuthStore } from "src/stores"
+import { fetchWrapper } from "src/helpers"
+import BackButton from "src/components/shared/buttons/BackButton.vue"
+import Actions from "src/components/shared/forms/FormCardActions.vue"
 
-  const emit = defineEmits(["submitted"])
-  const authStore = useAuthStore()
+const emit = defineEmits(["submitted"])
+const authStore = useAuthStore()
 
-  const form = ref(null)
-  const oldPassword = ref("")
-  const newPassword = ref("")
-  const confirmNewPassword = ref("")
+const form = ref(null)
+const oldPassword = ref("")
+const newPassword = ref("")
+const confirmNewPassword = ref("")
 
-  const isPwdOldPassword = ref(true)
-  const isPwdNewPassword = ref(true)
-  const isPwdConfirmPassword = ref(true)
+const isPwdOldPassword = ref(true)
+const isPwdNewPassword = ref(true)
+const isPwdConfirmPassword = ref(true)
 
-  async function submitForm() {
-    await form.value.validate()
-      .then((success) => {
-        if (success) {
-          changePassword()
-        } else {
-          //todo: how to show validation message to user
-          alert("validation error")
-        }
-      })
-  }
+async function submitForm() {
+  await form.value.validate().then((success) => {
+    if (success) {
+      changePassword()
+    } else {
+      //todo: how to show validation message to user
+      alert("validation error")
+    }
+  })
+}
 
-  async function changePassword() {
-    await fetchWrapper
-      .post("scr/users/changePassword", {
-        id: authStore.user.id,
-        oldPassword: oldPassword.value,
-        password: newPassword.value,
-        confirmPassword: confirmNewPassword.value
-      })
-      .then((response) => {
-        emit("submitted", response)
-      })
-      .finally(() => {
-      })
-  }
+async function changePassword() {
+  await fetchWrapper
+    .post("scr/users/changePassword", {
+      id: authStore.user.id,
+      oldPassword: oldPassword.value,
+      password: newPassword.value,
+      confirmPassword: confirmNewPassword.value
+    })
+    .then((response) => {
+      emit("submitted", response)
+    })
+    .finally(() => {})
+}
 </script>
 
 <style scoped>
-  .change-password-card {
-    width: 300px;
-    /* border-radius: 9px; */
-  }
+.change-password-card {
+  width: 300px;
+  /* border-radius: 9px; */
+}
 
-  /* .change-password-btn {
+/* .change-password-btn {
     height: 40px;
     width: 60%;
   }
