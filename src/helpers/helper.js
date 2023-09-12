e8d3920daac6b4eb8e4ec5c35f4cb9fac76b7bd2
Khashayar Shomali <shomalikhashayar@gmail.com>
2023-09-12 15:19:51 +0330

    Review.

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -2,7 +2,7 @@ export const helper = {
   getEnumOptions(obj) {
     let list = []
     Object.keys(obj).forEach(key => {
-      //todo: translate key $t(shared.labels.key)
+      //todo: translate key $t(`shared.labels.${key}`)
       list.push({ label: key, value: obj[key] })
     })
     return list
