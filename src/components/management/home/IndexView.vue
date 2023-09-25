9707e07f4bdeecf04a3c24b06814d635a61ddaab
Khashayar Shomali <shomalikhashayar@gmail.com>
2023-09-25 17:42:43 +0330

    InvoicesWidget.

--- src/components/management/home/IndexView.vue
+++ src/components/management/home/IndexView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="q-gutter-y-lg">
-    <bar-chart />
+    <invoices-widget />
+  </div>
+  <div class="q-gutter-y-lg">
     <line-chart />
+    <bar-chart />
     <div class="row q-gutter-x-lg">
       <doughnut-chart />
       <pie-chart />
@@ -14,4 +17,16 @@ import BarChart from 'src/components/shared/Charts/BarChart.vue'
 import LineChart from 'src/components/shared/Charts/LineChart.vue'
 import DoughnutChart from 'src/components/shared/Charts/PieChart.vue'
 import PieChart from 'src/components/shared/Charts/DoughnutChart.vue'
+import InvoicesWidget from 'src/components/shared/Widgets/InvoicesWidget.vue'
 </script>
+
+<style lang="scss">
+.bordered-btn {
+  border: 1px solid #d9dce5;
+  box-shadow: 0 0 2px 0 #dedede;
+}
+
+.actions {
+  max-width: calc(100% - 68px);
+}
+</style>
