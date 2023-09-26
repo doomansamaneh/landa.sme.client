33801e17a126994f467037793816b5580b3a0b77
Mohammad Yengimolki <gammadesk@gmail.com>
2023-09-26 13:34:43 +0330

    use culture store

--- src/components/shared/SwitchLanguage.vue
+++ src/components/shared/SwitchLanguage.vue
@@ -2,8 +2,8 @@
   <q-btn-dropdown
     no-caps
     icon="language"
-    :label="selectedLanguageLabel"
-    :options="languageOptions"
+    :label="cultureStore.culture.value.name"
+    :options="cultures"
     class="lang-switcher text-primary text-bold"
     auto-close
   >
@@ -11,13 +11,13 @@
       <q-item
         class="z-max"
         clickable
-        v-for="(lang, index) in supportedLanguages"
+        v-for="(lang, index) in cultures"
         :key="index"
       >
         <q-item-section @click="switchLanguage(lang.code)">
           <q-item-label>{{ lang.name }}</q-item-label>
         </q-item-section>
-        <q-item-section v-if="currentLanguage === lang.code">
+        <q-item-section v-if="cultureStore.culture.value.code === lang.code">
           <q-icon
             name="done"
             color="primary"
@@ -33,56 +33,27 @@
 <script setup>
 import { ref, computed, watch, onMounted } from "vue"
 import { cultures } from "src/constants/enums"
+import { useCulture } from "src/composables/useCulture";
 import { useI18n } from "vue-i18n"
 
-const { locale } = useI18n({ useScope: "global" })
+const cultureStore = useCulture()
 
-const supportedLanguages = cultures
-
-const currentLanguage = ref(localStorage.getItem("selectedLanguage") || "fa-IR")
-
-const selectedLanguageLabel = computed(() => {
-  const currentLanguageCode = currentLanguage.value;
-  const culture = cultures.find(culture => culture.code === currentLanguageCode);
-  return culture ? culture.name : "";
-});
+//const { locale } = useI18n({ useScope: "global" })
 
 function switchLanguage(code) {
-  const oldLanguage = currentLanguage.value
-  currentLanguage.value = code
-  locale.value = code
-
-  const selectedLang = supportedLanguages.find((l) => l.code === code)
-
-  localStorage.setItem("selectedLanguage", code)
-  localStorage.setItem("languageDirection", selectedLang.dir)
-
-  document.documentElement.setAttribute("dir", selectedLang.dir)
-  document.documentElement.lang = code
-
-  document.body.classList.remove(`digits--${oldLanguage}`)
-  document.body.classList.add(`digits--${selectedLang.code}`)
-
-  refreshPage()
+  //locale.value = code
+  cultureStore.setCulture(code)
 }
 
-// Refresh the page
-const refreshPage = () => {
-  window.location.reload()
-}
+// watch(locale, (newValue) => {
+//   localStorage.setItem("selectedLanguage", newValue)
+// })
 
-watch(locale, (newValue) => {
-  localStorage.setItem("selectedLanguage", newValue)
-})
+// onMounted(() => {
+//   setDigits()
+// })
 
-onMounted(() => {
-  setDigits()
-})
-
-function setDigits() {
-  const savedLanguage = localStorage.getItem("selectedLanguage")
-  if (savedLanguage) {
-    document.body.classList.add(`digits--${savedLanguage}`)
-  }
-}
+// function setDigits() {
+//   document.body.classList.add(`digits--${cultureStore.culture.value.code}`)
+// }
 </script>
--- src/composables/useCulture.js
+++ src/composables/useCulture.js
@@ -1,14 +1,35 @@
 import { computed } from "vue"
 import { cultures } from "src/constants/enums"
+import { Loading } from "quasar"
 
 export function useCulture() {
-  const culture = computed(() => {
+  const getCulture = () => {
     const currentLanguage = localStorage.getItem("selectedLanguage") || "fa-IR"
     const defaultCulture = cultures.code = "en"
     return cultures.filter((culture) => culture.code === currentLanguage)[0] ?? defaultCulture
-  })
+  }
+
+  const culture = computed(() => getCulture())
+
+  const setCulture = (code) => {
+    document.body.classList.remove(`digits--${culture.value.code}`)
+    localStorage.setItem("selectedLanguage", code)
+
+    applyCulture()
+  }
+
+  const applyCulture = () => {
+    const lang = getCulture()
+    document.documentElement.setAttribute("dir", lang.dir)
+    document.documentElement.lang = lang.code
+    document.body.classList.add(`digits--${lang.code}`)
+
+    //todo: Refresh the page, how oto remove refresh
+    window.location.reload()
+  }
 
   return {
-    culture
+    culture,
+    setCulture
   }
 }
