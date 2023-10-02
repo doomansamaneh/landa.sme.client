26035b898906e11bb91f807ac2d39cec7ef9caa6
Khashayar Shomali <shomalikhashayar@gmail.com>
2023-10-02 20:42:44 +0330

    Date in header.

--- src/components/shared/TodayDate.vue
+++ src/components/shared/TodayDate.vue
@@ -1,19 +1,22 @@
 <template>
-  <q-page-sticky
+  <!-- <q-page-sticky
     class="z-1 q-pa-sm"
     position="bottom-right"
-  >
-    <q-badge class="q-px-sm q-py-sm"><span class="text-body2">{{ $t('shared.labels.today') }}: {{
-      date.toLocaleDateString("fa-IR", options) }}</span>
-    </q-badge>
-  </q-page-sticky>
+  > -->
+  <span class="text-caption text-on-dark">{{ $t('shared.labels.today') }}: {{
+    date.toLocaleDateString("fa-IR", weekday) }} - {{ date.toLocaleDateString("fa-IR", fullDate) }}</span>
+  <!-- </q-page-sticky> -->
 </template>
 
 <script setup>
 const date = new Date()
-const options = {
+const fullDate = {
   year: "numeric",
   month: "numeric",
   day: "numeric"
+}
+
+const weekday = {
+  weekday: "long"
 }
 </script>
