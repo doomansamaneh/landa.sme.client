1cf5bd889fc2e10e974bcda628331ec8dfd31c54
Khashayar Shomali <shomalikhashayar@gmail.com>
2023-11-15 15:23:25 +0330

    ContactDrawer infinite scroll loader.

--- src/components/layouts/main/ContactDrawer.vue
+++ src/components/layouts/main/ContactDrawer.vue
@@ -34,102 +34,98 @@
       :thumb-style="helper.thumbStyle"
       :bar-style="helper.barStyle"
     >
-      <q-list ref="scrollTargetRef">
-        <q-infinite-scroll
-          @load="onLoadRef"
-          :offset="250"
-          :scroll-target="scrollTargetRef"
+      <q-infinite-scroll
+        @load="onLoadRef"
+      >
+        <q-item
+          @click="goToCustomer"
+          v-for="(row, index) in rows"
+          :key="index"
+          clickable
+          v-close-popup
+          class="q-py-sm text-on-dark"
         >
-          <q-item
-            @click="goToCustomer"
-            v-for="(row, index) in rows"
-            :key="index"
-            clickable
-            v-close-popup
-            class="q-py-sm text-on-dark"
-          >
-            <div class="row q-gutter-x-md items-center q-my-md q-pl-sm">
-              <q-item-section avatar>
-                <q-avatar
-                  v-if="row.avatar"
-                  color="primary"
-                  text-color="white"
-                  size="52px"
-                >
-                  <img :src="row.avatar">
-                </q-avatar>
-
-                <q-avatar
-                  size="52px"
-                  color="primary"
-                  text-color="white"
-                  v-else
-                >
-                  <div class="char text-body1 text-bold">
-                    {{ helper.getFirstChar(row.name) }}
-                  </div>
-                </q-avatar>
-              </q-item-section>
-
-              <q-item-section>
-                <q-item-label
-                  class="ellipsis text-on-dark text-caption text-bold q-py-xs"
-                  style="width:200px"
-                >
-                  {{ row.code }} {{ row.name }}
-                </q-item-label>
-                <q-item-label
-                  v-if="row.locationName"
-                  caption
-                  class="ellipsis q-mt-xs"
-                  style="width: 200px;"
-                >
-                  <q-icon
-                    name="o_location_on"
-                    size="13px"
-                    color="primary"
-                  />
-                  {{ row.locationName }} {{ row.address }}
-                </q-item-label>
-                <q-item-label
-                  caption
-                  class="flex justify-end q-gutter-x-xs rtl"
-                  v-if="row.phoneNo"
-                >
-                  {{ helper.separatePhoneNumbers(row.phoneNo) }}
-                  <q-icon
-                    name="o_phone"
-                    class="q-mr-xs"
-                    color="primary"
-                    size="13px"
-                  />
-                </q-item-label>
-                <q-item-label
-                  caption
-                  class="flex justify-end q-gutter-x-xs rtl"
-                  v-if="row.mobileNo"
-                >
-                  {{ helper.separatePhoneNumbers(row.mobileNo) }}
-                  <q-icon
-                    name="o_phone_android"
-                    class="q-mr-xs"
-                    color="primary"
-                    size="13px"
-                  />
-                </q-item-label>
-              </q-item-section>
-            </div>
-          </q-item>
-          <template v-slot:loading>
-            <div class="row justify-center q-my-md">
-              <q-spinner-dots
+          <div class="row q-gutter-x-md items-center q-my-md q-pl-sm">
+            <q-item-section avatar>
+              <q-avatar
+                v-if="row.avatar"
                 color="primary"
-                size="40px"
-              />
-            </div>
-          </template>
-        </q-infinite-scroll>
-      </q-list>
+                text-color="white"
+                size="52px"
+              >
+                <img :src="row.avatar">
+              </q-avatar>
+
+              <q-avatar
+                size="52px"
+                color="primary"
+                text-color="white"
+                v-else
+              >
+                <div class="char text-body1 text-bold">
+                  {{ helper.getFirstChar(row.name) }}
+                </div>
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label
+                class="ellipsis text-on-dark text-caption text-bold q-py-xs"
+                style="width:200px"
+              >
+                {{ row.code }} {{ row.name }}
+              </q-item-label>
+              <q-item-label
+                v-if="row.locationName"
+                caption
+                class="ellipsis q-mt-xs"
+                style="width: 200px;"
+              >
+                <q-icon
+                  name="o_location_on"
+                  size="13px"
+                  color="primary"
+                />
+                {{ row.locationName }} {{ row.address }}
+              </q-item-label>
+              <q-item-label
+                caption
+                class="flex justify-end q-gutter-x-xs rtl"
+                v-if="row.phoneNo"
+              >
+                {{ helper.separatePhoneNumbers(row.phoneNo) }}
+                <q-icon
+                  name="o_phone"
+                  class="q-mr-xs"
+                  color="primary"
+                  size="13px"
+                />
+              </q-item-label>
+              <q-item-label
+                caption
+                class="flex justify-end q-gutter-x-xs rtl"
+                v-if="row.mobileNo"
+              >
+                {{ helper.separatePhoneNumbers(row.mobileNo) }}
+                <q-icon
+                  name="o_phone_android"
+                  class="q-mr-xs"
+                  color="primary"
+                  size="13px"
+                />
+              </q-item-label>
+            </q-item-section>
+          </div>
+        </q-item>
+        <template v-slot:loading>
+          <div class="row justify-center">
+            <q-spinner-dots
+              color="primary"
+              size="40px"
+            />
+          </div>
+        </template>
+      </q-infinite-scroll>
     </q-scroll-area>
   </q-drawer>
 </template>
@@ -146,11 +142,9 @@ const customerStore = useCustomer()
 const contactDrawerStore = useContactDrawer()
 
 const tableStore = useDataTable("crm/customer/getLookupData", customerStore.columns, customerStore)
-
 const router = useRouter()
 
 const rows = ref([])
-const scrollTargetRef = ref(null)
 
 onMounted(() => {
   //todo:
@@ -176,7 +170,7 @@ const goToCustomer = () => {
 }
 
 const onLoadRef = async (index, done) => {
-  // console.log(index)
+  console.log(index)
   if (tableStore.pagination.value.totalItems > rows.value.length) {
     await gotoNext(index)
   }
