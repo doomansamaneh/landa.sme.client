134bd968a1fc1bc0f5a72bf4970da56d5d8a8817
Mohammad Yengimolki <gammadesk@gmail.com>
2023-11-27 10:18:30 +0330

    no product found message

--- src/components/areas/sls/invoice/create/NoProductSelected.vue
+++ src/components/areas/sls/invoice/create/NoProductSelected.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="column items-center q-gutter-md q-py-md">
-        <img
-          src="/productshelf.svg"
-          style="width: 120px"
-          alt=""
-        >
-        <div class="text-body1">
-          کالا یا خدمتی یافت نشد!
-        </div>
+    <img
+      src="/productshelf.svg"
+      style="width: 120px"
+      alt=""
+    >
+    <div class="text-body1">
+      کالا یا خدمتی انتخاب نشده است
+    </div>
   </div>
 </template>
 
