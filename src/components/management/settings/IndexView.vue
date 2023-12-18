b01fc26631b07f54e28124975b0fb158f22c50f3
Khashayar Shomali <shomalikhashayar@gmail.com>
2023-12-18 13:56:23 +0330

    Settings Page.

--- src/components/management/settings/IndexView.vue
+++ src/components/management/settings/IndexView.vue
@@ -1,71 +1,20 @@
 <template>
-  <q-tabs
-    v-model="tab"
-    class="border-radius-xs bg-primary text-white q-mt-md"
-    indicator-color="white"
-    align="justify"
-    inline-label
-    narrow-indicator
-  >
-    <q-tab
-      name="profile"
-      label="حساب"
-      icon="o_person"
-    />
-    <q-tab
-      name="quotes"
-      label="رنگ تم"
-      icon="o_palette"
-    />
-    <q-tab
-      name="invoices"
-      label="تب تستی 1"
-      icon="o_circle"
-    />
-    <q-tab
-      name="accountSummary"
-      label="تبت تستی 2"
-      icon="o_circle"
-    />
-
-  </q-tabs>
-
-  <q-tab-panels
-    class="bordered border-radius-xs q-mt-md"
-    v-model="tab"
-    animated
-  >
-    <q-tab-panel name="profile">
-      <div class="row">
-        <div class="col-4">
+  <div class="flex flex-center">
+    <q-card class="bordered card-width">
+      <q-card-section class="q-pa-lg">
+        <div class="text-body1">یک رنگ برای تم خود انتخاب کنید</div>
+        <div class="row items-center q-pt-md">
           <theme-selector />
+        </div>
+      </q-card-section>
+      <q-card-section class="q-pa-lg">
+        <div class="text-body1">زبان مورد نظر خود را انتخاب کنید</div>
+        <div class="row items-center q-pt-md">
           <lang-switcher />
         </div>
-        <q-separator
-          vertical
-          inset
-          class="q-mx-lg"
-        />
-        <div class="col"></div>
-      </div>
-    </q-tab-panel>
-
-    <q-tab-panel name="test1">
-
-    </q-tab-panel>
-
-    <q-tab-panel name="test2">
-
-    </q-tab-panel>
-
-    <q-tab-panel name="test3">
-
-    </q-tab-panel>
-
-    <q-tab-panel name="test4">
-
-    </q-tab-panel>
-  </q-tab-panels>
+      </q-card-section>
+    </q-card>
+  </div>
 </template>
 
 <script setup>
@@ -73,18 +22,12 @@ import { ref, onMounted, computed } from "vue"
 import ThemeSelector from "src/components/shared/ThemeSelector.vue"
 import LangSwitcher from "src/components/shared/SwitchLanguage.vue"
 
-const tab = ref('profile')
 
 </script>
 
-<style lang="scss">
-.profile-section {
-  width: 400px;
-}
-
-.info-box:hover {
-  border: 1px solid var(--q-primary) !important;
-  cursor: pointer;
+<style lang="scss" scoped>
+.card-width {
+  width: 970px;
 }
 </style>
 
