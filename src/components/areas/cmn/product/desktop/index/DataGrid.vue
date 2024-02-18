282dfede9f30ecdaaaa1fc103d0f6ea87b9af3e0
Khashayar Shomali <shomalikhashayar@gmail.com>
2024-02-18 14:00:22 +0330

    Desktop Product's DataGrid.

--- src/components/areas/cmn/product/desktop/index/DataGrid.vue
+++ src/components/areas/cmn/product/desktop/index/DataGrid.vue
@@ -1,47 +1,159 @@
-<script setup>
-import { ref } from 'vue';
-import ApexChart from 'vue3-apexcharts';
-
-const chartOptions = {
-  chart: {
-    id: 'vuechart-example',
-  },
-  xaxis: {
-    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-  },
-};
-
-const series = ref([
-  {
-    name: 'Vue Chart',
-    data: [300, 40, 45, 50, 49, 60, 70, 81],
-  },
-]);
-
-const updateChart = () => {
-  const max = 90;
-  const min = 20;
-  const newData = series.value[0].data.map(() => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  });
-
-  // Update the series option
-  series.value[0].data = newData;
-};
-</script>
-
 <template>
-  <div class="text-h1 text-center">quasar with vite
-    <apex-chart
-      type="line"
-      height="350"
-      :options="chartOptions"
-      :series="series"
-    ></apex-chart>
-    <q-btn color="red" @click="updateChart" label="Update Chart"></q-btn>
+  <tool-bar
+    :table-store="dataGrid?.tableStore"
+    :crud-store="crudStore"
+    :title="title"
+    base-route="/cmn/product"
+    activation
+  >
+    <template #buttons-batch-action>
+      <q-btn
+        class="text-caption"
+        rounded
+        unelevated
+        no-caps
+        @click="editBatch"
+      >
+        <q-icon
+          name="o_edit"
+          class="q-mr-xs"
+        />
+        {{ $t("shared.labels.editBatch") }} ({{ selectedIds?.length }} مورد)
+      </q-btn>
+    </template>
+  </tool-bar>
+  <!-- ----------------------------------------------- -->
+  <!-- Persian tools library tools:
+  <br />
+  <a href="https://github.com/persian-tools/persian-tools">persian tools</a>
+  <input
+    type="number"
+    v-model="numbersInput"
+  />
+  <p>Result: {{ convertedNumber }}</p> -->
+  <!-- ---------------------------------------------- -->
+  <div>
+    <!-- <q-card-section class="bg-primary text-white">
+      <div class="text-h5">{{ title }}</div>
+    </q-card-section> -->
+    <div>
+      <data-grid
+        ref="dataGrid"
+        dataSource="cmn/product/getGridData"
+        :grid-store="gridStore"
+        separator="horizontal"
+        flat
+        multiSelect
+        numbered
+        bordered
+        wrapCells
+        dense_
+        expandable_
+      >
+        <template #filter-isActive="{ col }">
+          <custom-select
+            v-model="col.value"
+            :options="isActiveOptions"
+            @update:model-value="reloadData"
+          />
+        </template>
+
+        <template #cell-code="{ item }">
+          {{ item.code }}
+          <div v-if="item.taxCode">
+            <small class="caption-on-dark">
+              tax: {{ item.taxCode }}
+            </small>
+          </div>
+        </template>
+
+        <template #cell-isActive="{ item }">
+          <q-btn
+            v-if="item.isActive"
+            round
+            dense
+            size="10px"
+            unelevated
+            icon="o_done"
+            color="green-8"
+            class="green-shadow no-pointer-events"
+          />
+          <q-btn
+            v-else
+            round
+            dense
+            size="10px"
+            unelevated
+            icon="o_close"
+            color="green-8"
+            class="green-shadow no-pointer-events"
+          />
+        </template>
+
+        <template #cell-actions="{ item }">
+          <row-tool-bar
+            base-route="/cmn/product"
+            :item="item"
+            :table-store="tableStore"
+            :crud-store="crudStore"
+          />
+        </template>
+
+      </data-grid>
+    </div>
   </div>
 </template>
 
-<style scoped>
-/* Add your component-specific styles here */
-</style>
+<script setup>
+import { ref, computed, watchEffect } from "vue"
+import { useQuasar } from "quasar"
+import { isActiveOptions } from "src/constants"
+import { useFormActions } from "src/composables/useFormActions"
+
+/////////////////////////////////////////////////////////////////
+import { numberToWords } from "@persian-tools/persian-tools";
+//////////////////////////////////////////////////////////////////
+
+import ToolBar from "src/components/shared/ToolBar.vue"
+import RowToolBar from "src/components/shared/RowToolBar.vue"
+import CustomSelect from "src/components/shared/forms/CustomSelect.vue"
+import DataGrid from "src/components/shared/dataTables/desktop/DataGrid.vue"
+import EditBatchDialog from "src/components/areas/cmn/product/shared/forms/EditBatchDialog.vue"
+
+/////////////////////////////////
+const numbersInput = ref(1379);
+const convertedNumber = ref();
+
+watchEffect(() => {
+  const converted = numberToWords(numbersInput.value);
+  convertedNumber.value = converted;
+});
+////////////////////////////////////
+
+const props = defineProps({
+  gridStore: Object,
+  title: String
+})
+
+const dataGrid = ref(null)
+const crudStore = useFormActions("cmn/product")
+const $q = useQuasar()
+
+async function reloadData() {
+  await tableStore.value.reloadData()
+}
+
+function editBatch() {
+  $q.dialog({
+    component: EditBatchDialog,
+    componentProps: {
+      selectedIds: selectedIds?.value,
+    }
+  }).onOk(async () => {
+    await reloadData()
+  })
+}
+
+const tableStore = computed(() => dataGrid.value?.tableStore)
+const selectedIds = computed(() => tableStore.value?.selectedRows?.value.map(item => item.id))
+</script>
