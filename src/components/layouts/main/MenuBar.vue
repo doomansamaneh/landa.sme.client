6919d9b08b6e707c98fddd4c6d39c86121db2bad
Khashayar Shomali <shomalikhashayar@gmail.com>
2024-04-16 15:37:32 +0330

    New MenuBar.

--- src/components/layouts/main/MenuBar.vue
+++ src/components/layouts/main/MenuBar.vue
@@ -1,6 +1,6 @@
 <template>
   <q-drawer class="menu-bar q-mb-xl z-max" :class="$q.dark.isActive ? 'bg-dark' : 'bg-light'" side="left" bordered
-    :width="247" v-model="menuBarStore.state.visible.value" :breakpoint="400" :overlay="$q.screen.lt.md">
+    :width="285" v-model="menuBarStore.state.visible.value" :breakpoint="400" :overlay="$q.screen.lt.md">
     <div class="flex justify-center q-px-md q-py-lg">
       <q-input inputmode="search" color="grey-5" outlined v-model="menuBarStore.searchText.value"
         :placeholder="$t('main-menu-items.search')" dense rounded clearable clear-icon="o_close"
@@ -12,29 +12,32 @@
     </div>
 
     <q-scroll-area style="height: calc(100% - 120px);" :thumb-style="helper.thumbStyle" :bar-style="helper.barStyle">
-      <q-list class="menu-list">
-        <q-item class="first-item flex text-body2 no-lette-spacing items-center cursor-pointer q-mr-md" to="/dashboard">
+      <q-list class="menu-list q-px-md">
+        <q-item class="border-radius-xl first-item q-mb-xs flex text-body2 no-lette-spacing items-center cursor-pointer" to="/dashboard">
           <q-icon name="o_dashboard" class="settings q-mr-sm" size="xs"></q-icon>
           {{ $t("main-menu-items.dashboard") }}
         </q-item>
         <div v-for="parentItem in menuBarStore.drawerMenuItems.value" :key="parentItem.name">
           <q-expansion-item ref="expansion" group="menu" :label="parentItem.title" :icon="`o_${parentItem.icon}`"
-            class="text-body2 no-letter-spacing" :header-class="{
+            class="parent text-body2 no-letter-spacing" :header-class="{
               'text-yellow text-bold': shouldHighlight(parentItem.subItems) && $q.dark.isActive,
               'text-primary text-bold': shouldHighlight(parentItem.subItems) && !$q.dark.isActive
             }">
-            <q-item v-for="subItem in parentItem.subItems" :key="subItem.name" :to="subItem.url" clickable
-              class="sub-item q-mr-lg">
-              <q-item-section avatar class="item-section">
-                <q-icon :name="`o_${subItem.icon}`" size="xs" />
-              </q-item-section>
-              <q-item-section>
-                <span class="text-body2 sub no-letter-spacing"> {{ subItem.title }} </span>
-              </q-item-section>
-            </q-item>
+            <div class="sub-item-container q-ml-lg">
+              <q-item v-for="subItem in parentItem.subItems" :key="subItem.name" :to="subItem.url" clickable
+                class="border-radius-xl sub-item q-ml-xs q-my-xs">
+                <q-item-section avatar class="">
+                  <q-icon :name="`o_${subItem.icon}`" size="xs" />
+                </q-item-section>
+                <q-item-section>
+                  <span class="text-body2 sub no-letter-spacing"> {{ subItem.title }} </span>
+                </q-item-section>
+              </q-item>
+            </div>
           </q-expansion-item>
         </div>
-        <q-item to="/cmn/appConfig" class="last-item flex text-body2 no-lette-spacing items-center cursor-pointer q-mr-md">
+        <q-item to="/cmn/appConfig"
+          class="last-item border-radius-xl flex text-body2 no-lette-spacing items-center cursor-pointer q-my-xs">
           <q-icon name="o_settings" class="settings q-mr-sm" size="xs"></q-icon>
           {{ $t("main-menu-items.settings") }}
         </q-item>
@@ -72,14 +75,8 @@ onMounted(() => {
   font-size: 13px;
 }
 
-.menu-bar {
-  .q-focus-helper {
-    display: none;
-  }
-}
-
 .q-item__section--side {
-  padding-right: 8px;
+  // padding-right: 8px;
 
   .q-icon {
     font-size: 20px;
