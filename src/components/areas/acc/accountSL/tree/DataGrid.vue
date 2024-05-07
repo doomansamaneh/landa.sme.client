cd65e32b6bc83a08b012be17097c7ff041103053
Khashayar Shomali <shomalikhashayar@gmail.com>
2024-05-07 12:55:39 +0330

    Clean AccountSL tree.

--- src/components/areas/acc/accountSL/tree/DataGrid.vue
+++ src/components/areas/acc/accountSL/tree/DataGrid.vue
@@ -2,142 +2,138 @@
   <tool-bar margin title="سرفصل حسابها" />
 
   <q-card class="form-container">
-    <q-card-section class="q-pa-lg">
-      <div v-if="true">
-        <q-tree
-          :nodes="nodes"
-          @lazy-load="onLazyLoad"
-          node-key="id"
-          node-icon="search"
-          accordion
-          selected-color="primary"
-          icon="o_arrow_forward_ios"
-        >
-          <template #default-header="prop">
-            <div class="row items-center text-body2 no-letter-spacing q-pa-sm">
-              <div class="text-body2 no-letter-spacing">
-                {{ prop.node.code }} - {{ prop.node.title }}
-              </div>
+    <q-card-section>
+      <q-tree
+        :nodes="nodes"
+        @lazy-load="onLazyLoad"
+        node-key="id"
+        node-icon="search"
+        accordion
+        selected-color="primary"
+        icon="o_arrow_forward_ios"
+      >
+        <template #header-cl="prop">
+          <div class="row items-center text-body2 no-letter-spacing q-pa-sm">
+            <div class="text-body2 no-letter-spacing">
+              {{ prop.node.label }} - {{ prop.node.title }}
             </div>
-            <q-space />
-            <div class="row items-center q-gutter-md">
-              <q-btn @click.stop="menu = true" dense round unelevated icon="o_more_horiz">
-                <q-menu v-show="menu" class="border-radius-lg" fit :offset="[0, 10]">
-                  <q-list dense padding style="width: 200px">
-                    <q-item clickable v-close-popup tabindex="0">
-                      <div class="q-py-sm">
-                        <q-item-section avatar>
-                          <q-avatar class="bg-on-dark" size="sm">
-                            <q-icon size="20px" name="o_add" />
-                          </q-avatar>
-                        </q-item-section>
-                      </div>
-                      <q-item-section>
-                        <div class="text-body2 no-letter-spacing">ایجاد حساب کل</div>
+          </div>
+          <q-space />
+          <div class="row items-center q-gutter-md">
+            <q-btn @click.stop="menu = true" dense round unelevated icon="o_more_horiz">
+              <q-menu v-show="menu" class="border-radius-lg" fit :offset="[0, 10]">
+                <q-list dense padding style="width: 200px">
+                  <q-item clickable v-close-popup tabindex="0">
+                    <div class="q-py-sm">
+                      <q-item-section avatar>
+                        <q-avatar class="bg-on-dark" size="sm">
+                          <q-icon size="20px" name="o_add" />
+                        </q-avatar>
                       </q-item-section>
-                    </q-item>
-                  </q-list>
-                </q-menu>
-              </q-btn>
+                    </div>
+                    <q-item-section>
+                      <div class="text-body2 no-letter-spacing">ایجاد حساب کل</div>
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </q-btn>
+          </div>
+        </template>
+        <template #header-gl="prop">
+          <div class="row items-center q-gutter-md no-letter-spacing q-pa-sm">
+            <q-btn size="8px" round unelevated class="no-pointer-events" color="green">
+              <q-icon color="white" name="o_check" size="16px" />
+            </q-btn>
+            <div class="text-body2 no-letter-spacing">
+              {{ prop.node.title }}
             </div>
-          </template>
-          <template #header-second="prop">
-            <div class="row items-center q-gutter-md no-letter-spacing q-pa-sm">
-              <q-btn size="8px" round unelevated class="no-pointer-events" color="green">
-                <q-icon color="white" name="o_check" size="16px" />
-              </q-btn>
-              <div class="text-body2 no-letter-spacing">
-                {{ prop.node.label }}
-              </div>
-            </div>
-            <q-space />
-            <div class="row items-center">
-              <q-btn @click.stop="menu = true" dense round unelevated icon="o_more_horiz">
-                <q-menu v-show="menu" class="border-radius-lg" fit :offset="[0, 10]">
-                  <q-list dense padding style="width: 200px">
-                    <q-item clickable v-close-popup tabindex="0">
-                      <div class="q-py-sm">
-                        <q-item-section avatar>
-                          <q-avatar class="bg-on-dark" size="sm">
-                            <q-icon size="20px" name="o_add" />
-                          </q-avatar>
-                        </q-item-section>
-                      </div>
-                      <q-item-section>
-                        <div class="text-body2 no-letter-spacing">ایجاد حساب معین</div>
+          </div>
+          <q-space />
+          <div class="row items-center">
+            <q-btn @click.stop="menu = true" dense round unelevated icon="o_more_horiz">
+              <q-menu v-show="menu" class="border-radius-lg" fit :offset="[0, 10]">
+                <q-list dense padding style="width: 200px">
+                  <q-item clickable v-close-popup tabindex="0">
+                    <div class="q-py-sm">
+                      <q-item-section avatar>
+                        <q-avatar class="bg-on-dark" size="sm">
+                          <q-icon size="20px" name="o_add" />
+                        </q-avatar>
                       </q-item-section>
-                    </q-item>
-                    <q-item clickable v-close-popup tabindex="0">
-                      <div class="q-py-sm">
-                        <q-item-section avatar>
-                          <q-avatar class="bg-on-dark" size="sm">
-                            <q-icon size="20px" name="o_edit" />
-                          </q-avatar>
-                        </q-item-section>
-                      </div>
-                      <q-item-section>
-                        <div class="text-body2 no-letter-spacing">ویرایش</div>
+                    </div>
+                    <q-item-section>
+                      <div class="text-body2 no-letter-spacing">ایجاد حساب معین</div>
+                    </q-item-section>
+                  </q-item>
+                  <q-item clickable v-close-popup tabindex="0">
+                    <div class="q-py-sm">
+                      <q-item-section avatar>
+                        <q-avatar class="bg-on-dark" size="sm">
+                          <q-icon size="20px" name="o_edit" />
+                        </q-avatar>
                       </q-item-section>
-                    </q-item>
-                  </q-list>
-                </q-menu>
-              </q-btn>
+                    </div>
+                    <q-item-section>
+                      <div class="text-body2 no-letter-spacing">ویرایش</div>
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </q-btn>
+          </div>
+        </template>
+        <template #header-sl="prop">
+          <div class="row items-center q-gutter-md no-letter-spacing q-pa-sm">
+            <q-btn size="8px" round unelevated class="no-pointer-events" color="green">
+              <q-icon color="white" name="o_check" size="16px" />
+            </q-btn>
+            <div class="text-body2 no-letter-spacing">
+              {{ prop.node.title }}
             </div>
-          </template>
-          <template #header-third="prop">
-            <div class="row items-center q-gutter-md no-letter-spacing q-pa-sm">
-              <q-btn size="8px" round unelevated class="no-pointer-events" color="green">
-                <q-icon color="white" name="o_check" size="16px" />
-              </q-btn>
-              <div class="text-body2 no-letter-spacing">
-                {{ prop.node.label }}
-              </div>
-            </div>
-            <q-space />
-            <div class="row items-center">
-              <q-btn @click.stop="menu = true" dense round unelevated icon="o_more_horiz">
-                <q-menu v-show="menu" class="border-radius-lg" fit :offset="[0, 10]">
-                  <q-list dense padding style="width: 200px">
-                    <q-item clickable v-close-popup tabindex="0">
-                      <div class="q-py-sm">
-                        <q-item-section avatar>
-                          <q-avatar class="bg-on-dark" size="sm">
-                            <q-icon size="20px" name="o_edit" />
-                          </q-avatar>
-                        </q-item-section>
-                      </div>
-                      <q-item-section>
-                        <div class="text-body2 no-letter-spacing">ویرایش</div>
+          </div>
+          <q-space />
+          <div class="row items-center">
+            <q-btn @click.stop="menu = true" dense round unelevated icon="o_more_horiz">
+              <q-menu v-show="menu" class="border-radius-lg" fit :offset="[0, 10]">
+                <q-list dense padding style="width: 200px">
+                  <q-item clickable v-close-popup tabindex="0">
+                    <div class="q-py-sm">
+                      <q-item-section avatar>
+                        <q-avatar class="bg-on-dark" size="sm">
+                          <q-icon size="20px" name="o_edit" />
+                        </q-avatar>
                       </q-item-section>
-                    </q-item>
-                    <q-item clickable v-close-popup tabindex="0">
-                      <div class="q-py-sm">
-                        <q-item-section avatar>
-                          <q-avatar class="bg-on-dark" size="sm">
-                            <q-icon size="20px" name="o_delete" />
-                          </q-avatar>
-                        </q-item-section>
-                      </div>
-                      <q-item-section>
-                        <div class="text-body2 no-letter-spacing">حذف</div>
+                    </div>
+                    <q-item-section>
+                      <div class="text-body2 no-letter-spacing">ویرایش</div>
+                    </q-item-section>
+                  </q-item>
+                  <q-item clickable v-close-popup tabindex="0">
+                    <div class="q-py-sm">
+                      <q-item-section avatar>
+                        <q-avatar class="bg-on-dark" size="sm">
+                          <q-icon size="20px" name="o_delete" />
+                        </q-avatar>
                       </q-item-section>
-                    </q-item>
-                  </q-list>
-                </q-menu>
-              </q-btn>
-            </div>
-          </template>
-        </q-tree>
-      </div>
+                    </div>
+                    <q-item-section>
+                      <div class="text-body2 no-letter-spacing">حذف</div>
+                    </q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </q-btn>
+          </div>
+        </template>
+      </q-tree>
     </q-card-section>
 
-    <q-card-section
-      v-if="!firstLevelStore.showLoader.value && firstLevelStore.rows.value.length == 0"
-    >
+    <q-card-section v-if="!clStore.showLoader.value && clStore.rows.value.length == 0">
       <no-data-found />
     </q-card-section>
 
-    <q-inner-loading color="primary" :showing="firstLevelStore.showLoader.value" />
+    <q-inner-loading color="primary" :showing="clStore.showLoader.value" />
   </q-card>
 </template>
 
@@ -158,60 +154,41 @@ import {
 import { sqlOperator } from "src/constants";
 import NoDataFoundVue from "src/components/shared/dataTables/NoDataFound.vue";
 
-const firstLevelDataSource = "acc/AccountCL/getGridData";
-const secondLevelDataSource = "acc/AccountGL/getGridData";
-const thirdLevelDataSource = "acc/AccountSL/getGridData";
+const clDataSource = "acc/AccountCL/getGridData";
+const glDataSource = "acc/AccountGL/getGridData";
+const slDataSource = "acc/AccountSL/getGridData";
 
-// const filterExpersions = computed(() => {
-//   if (filterExpersions.value) {
-//     return filterExpersions;
-//   } else {
-//     if (secondLevelRow.id) {
-//       return [
-//         {
-//           fieldName: "clId",
-//           operator: sqlOperator.equal,
-//           value: secondLevelRow.id,
-//         },
-//       ];
-//     }
-//   }
-
-//   return "";
-// });
-
-const firstLevelGridStore = useAccountCLGrid({
+const clGridStore = useAccountCLGrid({
   columns: accountCLColumns,
   sortColumn: "code",
 });
 
-const secondLevelGridStore = useAccountGLGrid({
+const glGridStore = useAccountGLGrid({
   columns: accountGLColumns,
   sortColumn: "code",
-  // filterExpersions: filterExpersions.value,
 });
 
-const thirdLevelGridStore = useAccountSLGrid({
+const slGridStore = useAccountSLGrid({
   columns: accountSLColumns,
   sortColumn: "code",
-  // filterExpersions: filterExpersions.value,
 });
 
 const nodes = ref([]);
 const menu = ref(false);
 
-const firstLevelStore = useDataTable(firstLevelDataSource, null, firstLevelGridStore);
-const secondLevelStore = useDataTable(secondLevelDataSource, null, secondLevelGridStore);
-const thirdLevelStore = useDataTable(thirdLevelDataSource, null, thirdLevelGridStore);
+const clStore = useDataTable(clDataSource, null, clGridStore);
+const glStore = useDataTable(glDataSource, null, glGridStore);
+const slStore = useDataTable(slDataSource, null, slGridStore);
 
 onMounted(() => {
   loadClData();
 });
 
 async function loadClData() {
-  firstLevelStore.pagination.value.pageSize = -1;
-  await firstLevelStore.loadData();
-  firstLevelStore.rows.value.forEach((element) => {
+  clStore.pagination.value.pageSize = -1;
+  await clStore.loadData();
+  clStore.rows.value.forEach((element) => {
+    element.header = "cl";
     element.level = "cl";
     element.filterExpression = [
       {
@@ -220,17 +197,17 @@ async function loadClData() {
         value: element.id,
       },
     ];
-    //element.children = [];
     element.lazy = true;
   });
-  nodes.value = firstLevelStore.rows.value;
+  nodes.value = clStore.rows.value;
 }
 
 async function loadGlData(node) {
-  secondLevelStore.pagination.value.pageSize = -1;
-  secondLevelStore.state.value.filterExpression = node.filterExpression;
-  await secondLevelStore.reloadData();
-  secondLevelStore.rows.value.forEach((element) => {
+  glStore.pagination.value.pageSize = -1;
+  glStore.state.value.filterExpression = node.filterExpression;
+  await glStore.reloadData();
+  glStore.rows.value.forEach((element) => {
+    element.header = "gl";
     element.level = "gl";
     element.filterExpression = [
       {
@@ -239,27 +216,30 @@ async function loadGlData(node) {
         value: element.id,
       },
     ];
-    //element.children = [];
     element.lazy = true;
   });
 }
 
 async function loadSlData(node) {
-  thirdLevelStore.pagination.value.pageSize = -1;
-  thirdLevelStore.state.value.filterExpression = node.filterExpression;
-  await thirdLevelStore.reloadData();
+  slStore.pagination.value.pageSize = -1;
+  slStore.state.value.filterExpression = node.filterExpression;
+  await slStore.reloadData();
+  slStore.rows.value.forEach((element) => {
+    element.header = "sl";
+    element.level = "sl";
+  });
 }
 
 const onLazyLoad = async ({ node, key, done, fail }) => {
   if (node.level === "cl") {
     await loadGlData(node);
-    alert(1);
-    done(secondLevelStore.rows.value);
+    done(glStore.rows.value);
   } else if (node.level === "gl") {
     await loadSlData(node);
-    done(thirdLevelStore.rows.value);
+    done(slStore.rows.value);
   } else {
     done([]);
   }
 };
+
 </script>
