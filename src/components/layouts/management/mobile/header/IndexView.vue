364fcdd10fa738d4377584dacc0966249c328bd9
Khashayar Shomali <shomalikhashayar@gmail.com>
2024-05-28 15:28:54 +0330

    Modifiying active color of q-items on -dark.

--- src/components/layouts/management/mobile/header/IndexView.vue
+++ src/components/layouts/management/mobile/header/IndexView.vue
@@ -1,10 +1,7 @@
 <template>
   <transition enter-active-class="animated slideInDown">
     <!-- v-show="headerStore.showHeader.value" -->
-    <q-header
-      bordered
-      class="business-header bg-main bg-dark"
-    >
+    <q-header bordered class="business-header bg-main bg-dark">
       <q-toolbar class="row q-px-lg">
         <div class="col-2 row items-center q-gutter-x-sm">
           <div>
@@ -24,11 +21,10 @@
         </div>
         <div class="col row items-center q-gutter-x-sm justify-end">
           <switch-theme />
-          <div class="flex items-center text-on-dark q-gutter-x-xs cursor-pointer">
-            <q-icon
-              size="sm"
-              name="o_account_circle"
-            />
+          <div
+            class="flex items-center text-on-dark q-gutter-x-xs cursor-pointer"
+          >
+            <q-icon size="sm" name="o_account_circle" />
             <div>{{ username }}</div>
             <q-menu
               fit
@@ -36,21 +32,18 @@
               transition-hide="jump-up"
               :offset="[10, 28]"
             >
-              <q-list
-                dense
-                padding
-                class="user-profile"
-              >
-                <q-item-label
-                  class="text-h6"
-                  header
-                >
-                  <div class="column items-center justify-center q-mt-md">
+              <q-list dense padding class="user-profile">
+                <q-item-label class="text-h6" header>
+                  <div
+                    class="column items-center justify-center q-mt-md"
+                  >
                     <q-avatar
-                      class="profile-pic q-mx-sm"
+                      class="profile-pic primary-shadow primary-gradient q-mx-sm"
                       text-color="white"
                     >
-                      <div class="text-bold">{{ helper.getFirstChar(username) }}</div>
+                      <div class="text-bold">
+                        {{ helper.getFirstChar(username) }}
+                      </div>
                     </q-avatar>
                     <div class="q-mt-md">
                       <span class="text-on-dark">{{ username }}</span>
@@ -75,7 +68,9 @@
                   </div>
 
                   <q-item-section>
-                    {{ $t("business-layout.buttons.change-password") }}
+                    {{
+                      $t("business-layout.buttons.change-password")
+                    }}
                   </q-item-section>
                 </q-item>
                 <q-item
@@ -104,10 +99,7 @@
         </div>
       </q-toolbar>
       <q-separator class="business-header" />
-      <q-toolbar
-        style="min-height: 40px;"
-        class="q-px-lg"
-      >
+      <q-toolbar style="min-height: 40px" class="q-px-lg">
         <bread-crumbs />
       </q-toolbar>
     </q-header>
@@ -115,67 +107,65 @@
 </template>
 
 <script setup>
-import { computed, onMounted, onUnmounted } from "vue"
-import { helper } from "src/helpers"
-import { useQuasar } from "quasar"
-import { useAuthStore } from "src/stores"
-import { useHeader } from "src/composables/mobile/useHeader"
+  import { computed, onMounted, onUnmounted } from "vue";
+  import { helper } from "src/helpers";
+  import { useQuasar } from "quasar";
+  import { useAuthStore } from "src/stores";
+  import { useHeader } from "src/composables/mobile/useHeader";
 
-import SwitchTheme from "src/components/shared/SwitchTheme.vue"
-import BreadCrumbs from "src/components/shared/BreadCrumbs.vue"
-import ChangePasswordDialog from "src/components/areas/scr/users/ChangePasswordDialog.vue"
+  import SwitchTheme from "src/components/shared/SwitchTheme.vue";
+  import BreadCrumbs from "src/components/shared/BreadCrumbs.vue";
+  import ChangePasswordDialog from "src/components/areas/scr/users/ChangePasswordDialog.vue";
 
-const $q = useQuasar()
-const authStore = useAuthStore()
-const headerStore = useHeader()
+  const $q = useQuasar();
+  const authStore = useAuthStore();
+  const headerStore = useHeader();
 
-const username = computed(() => {
-  if (authStore.user) return authStore.user.fullName
-  return ""
-})
+  const username = computed(() => {
+    if (authStore.user) return authStore.user.fullName;
+    return "";
+  });
 
-function changePasswordDialog() {
-  $q.dialog({
-    component: ChangePasswordDialog
-  })
-}
+  function changePasswordDialog() {
+    $q.dialog({
+      component: ChangePasswordDialog,
+    });
+  }
 
-// onMounted(() => {
-//   window.addEventListener('scroll', headerStore.handleScroll)
-// })
+  // onMounted(() => {
+  //   window.addEventListener('scroll', headerStore.handleScroll)
+  // })
 
-// onUnmounted(() => {
-//   window.removeEventListener('scroll', headerStore.handleScroll)
-// })
-
+  // onUnmounted(() => {
+  //   window.removeEventListener('scroll', headerStore.handleScroll)
+  // })
 </script>
 
 <style lang="scss">
-.logo {
-  filter: brightness(0) invert(1);
-}
+  .logo {
+    filter: brightness(0) invert(1);
+  }
 
-.q-toolbar__title {
-  font-size: 14px;
-  font-weight: 600;
-}
+  .q-toolbar__title {
+    font-size: 14px;
+    font-weight: 600;
+  }
 
-.navbar-logo {
-  text-decoration: none;
-  color: black;
-}
+  .navbar-logo {
+    text-decoration: none;
+    color: black;
+  }
 
-.user-profile {
-  min-width: 240px;
-}
+  .user-profile {
+    min-width: 240px;
+  }
 
-.profile-pic {
-  width: 72px;
-  height: 72px;
-}
+  .profile-pic {
+    width: 72px;
+    height: 72px;
+  }
 
-.q-breadcrumbs__el.items-center {
-  display: block;
-}
+  .q-breadcrumbs__el.items-center {
+    display: block;
+  }
 </style>
-
