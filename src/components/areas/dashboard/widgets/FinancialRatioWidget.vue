eb273a461e64f850fc57e3cfcabee28c0769a37f
Khashayar Shomali <shomalikhashayar@gmail.com>
2024-09-01 17:10:51 +0330

    RatiosWidget.

--- src/components/areas/dashboard/widgets/FinancialRatioWidget.vue
+++ src/components/areas/dashboard/widgets/FinancialRatioWidget.vue
@@ -2,13 +2,28 @@
   <div class="row q-gutter-md">
     <div class="col">
       <q-card class="border-radius-lg q-pa-sm bordered">
-        <q-card-section class="column justify-center items-center q-gutter-sm">
+        <q-card-section
+          class="column justify-center items-center q-gutter-sm"
+        >
           <div class="q-gutter-x-xs">
             <span class="text-h6 text-weight-700">نسبت جاری</span>
-            <q-icon @mouseover="showTooltip(0)" name="o_live_help" size="xs" class="icon-hover dark-3 cursor-pointer">
-              <q-tooltip no-parent-event @mouseout="hideTooltip(0)" v-model="tooltips[0]" max-width="500px"
-                transition-show="scale" transition-hide="scale" anchor="center middle" self="center middle"
-                class="glass text-on-dark">
+            <q-icon
+              @mouseover="showTooltip(0)"
+              name="o_live_help"
+              size="xs"
+              class="icon-hover dark-3 cursor-pointer"
+            >
+              <q-tooltip
+                no-parent-event
+                @mouseout="hideTooltip(0)"
+                v-model="tooltips[0]"
+                max-width="500px"
+                transition-show="scale"
+                transition-hide="scale"
+                anchor="center middle"
+                self="center middle"
+                class="glass text-on-dark"
+              >
                 <div class="text-body2 line-height q-pa-lg">
                   {{ nesbatJari }}
                 </div>
@@ -21,13 +36,28 @@
     </div>
     <div class="col">
       <q-card class="q-pa-sm bordered">
-        <q-card-section class="column justify-center items-center q-gutter-sm">
+        <q-card-section
+          class="column justify-center items-center q-gutter-sm"
+        >
           <div class="q-gutter-x-xs">
             <span class="text-body1">دوره وصول مطالبات</span>
-            <q-icon @mouseover="showTooltip(1)" name="o_live_help" size="xs" class="icon-hover dark-3 cursor-pointer">
-              <q-tooltip no-parent-event @mouseout="hideTooltip(1)" v-model="tooltips[1]" max-width="500px"
-                transition-show="scale" transition-hide="scale" anchor="center middle" self="center middle"
-                class="more glass text-on-dark">
+            <q-icon
+              @mouseover="showTooltip(1)"
+              name="o_live_help"
+              size="xs"
+              class="icon-hover dark-3 cursor-pointer"
+            >
+              <q-tooltip
+                no-parent-event
+                @mouseout="hideTooltip(1)"
+                v-model="tooltips[1]"
+                max-width="500px"
+                transition-show="scale"
+                transition-hide="scale"
+                anchor="center middle"
+                self="center middle"
+                class="more glass text-on-dark"
+              >
                 <div class="text-body2 line-height q-pa-lg">
                   {{ nesbatJari }}
                 </div>
@@ -40,32 +70,64 @@
     </div>
     <div class="col">
       <q-card class="q-pa-sm bordered">
-        <q-card-section class="column justify-center items-center q-gutter-sm">
+        <q-card-section
+          class="column justify-center items-center q-gutter-sm"
+        >
           <div class="q-gutter-x-xs">
             <span class="text-body1">حاشیه سود</span>
-            <q-icon @mouseover="showTooltip(2)" name="o_live_help" size="xs" class="icon-hover dark-3 cursor-pointer">
-              <q-tooltip no-parent-event @mouseout="hideTooltip(2)" v-model="tooltips[2]" max-width="500px"
-                transition-show="scale" transition-hide="scale" anchor="center middle" self="center middle"
-                class="more glass text-on-dark">
+            <q-icon
+              @mouseover="showTooltip(2)"
+              name="o_live_help"
+              size="xs"
+              class="icon-hover dark-3 cursor-pointer"
+            >
+              <q-tooltip
+                no-parent-event
+                @mouseout="hideTooltip(2)"
+                v-model="tooltips[2]"
+                max-width="500px"
+                transition-show="scale"
+                transition-hide="scale"
+                anchor="center middle"
+                self="center middle"
+                class="more glass text-on-dark"
+              >
                 <div class="text-body2 line-height q-pa-lg">
                   {{ nesbatJari }}
                 </div>
               </q-tooltip>
             </q-icon>
           </div>
-          <span class="text-bold text-h6 text-primary">32,456.50%</span>
+          <span class="text-bold text-h6 text-primary">
+            32,456.50%
+          </span>
         </q-card-section>
       </q-card>
     </div>
     <div class="col">
       <q-card class="q-pa-sm bordered">
-        <q-card-section class="column justify-center items-center q-gutter-sm">
+        <q-card-section
+          class="column justify-center items-center q-gutter-sm"
+        >
           <div class="q-gutter-x-xs">
             <span class="text-body1">نسبت بدهی</span>
-            <q-icon @mouseover="showTooltip(3)" name="o_live_help" size="xs" class="icon-hover dark-3 cursor-pointer">
-              <q-tooltip no-parent-event @mouseout="hideTooltip(3)" v-model="tooltips[3]" max-width="500px"
-                transition-show="scale" transition-hide="scale" anchor="center middle" self="center middle"
-                class="more glass text-on-dark">
+            <q-icon
+              @mouseover="showTooltip(3)"
+              name="o_live_help"
+              size="xs"
+              class="icon-hover dark-3 cursor-pointer"
+            >
+              <q-tooltip
+                no-parent-event
+                @mouseout="hideTooltip(3)"
+                v-model="tooltips[3]"
+                max-width="500px"
+                transition-show="scale"
+                transition-hide="scale"
+                anchor="center middle"
+                self="center middle"
+                class="more glass text-on-dark"
+              >
                 <div class="text-body2 line-height q-pa-lg">
                   {{ nesbatJari }}
                 </div>
@@ -80,32 +142,30 @@
 </template>
 
 <script setup>
-import { ref } from "vue"
-const tooltips = ref([false, false, false, false])
+  import { ref } from "vue";
+  const tooltips = ref([false, false, false, false]);
 
-const showTooltip = (index) => {
-  setTimeout(() => {
+  const showTooltip = (index) => {
+    setTimeout(() => {
+      tooltips.value[index] = true;
+    }, 700);
+  };
 
-    tooltips.value[index] = true
+  const hideTooltip = (index) => {
+    tooltips.value[index] = false;
+  };
 
-  }, 700);
-}
-
-const hideTooltip = (index) => {
-  tooltips.value[index] = false
-}
-
-const nesbatJari = "نسبت جاری از تقسیم داراییهای جاری بر بدهی‌های جاری حاصل می‌شود دارایی جاری به داراییهایی گفته می‌شود كه به سرعت و به سهولت به وجه نقد تبدیل می‌شود و بدهی جاری به بدهی‌هایی گفته می‌شود كه با سررسید كمتر از یک سال است. نسبت جاری بیشتر می‌تواند بیانگر وضعیت مطلوب نقدینگی باشد. اگر این عدد بزرگتر از 2 باشد مطلوب است و وام دهندگان بزرگ مانند بانکها به این کسب و کار نظر مساعدی خواهند داشت."
-
+  const nesbatJari =
+    "نسبت جاری از تقسیم داراییهای جاری بر بدهی‌های جاری حاصل می‌شود دارایی جاری به داراییهایی گفته می‌شود كه به سرعت و به سهولت به وجه نقد تبدیل می‌شود و بدهی جاری به بدهی‌هایی گفته می‌شود كه با سررسید كمتر از یک سال است. نسبت جاری بیشتر می‌تواند بیانگر وضعیت مطلوب نقدینگی باشد. اگر این عدد بزرگتر از 2 باشد مطلوب است و وام دهندگان بزرگ مانند بانکها به این کسب و کار نظر مساعدی خواهند داشت.";
 </script>
 
 <style lang="scss">
-.line-height {
-  line-height: 28px;
-  text-align: justify;
-}
+  .line-height {
+    line-height: 28px;
+    text-align: justify;
+  }
 
-.no-pointer-events {
-  pointer-events: all !important;
-}
+  .no-pointer-events {
+    pointer-events: all !important;
+  }
 </style>
--- src/components/areas/dashboard/widgets/_RatioWidget.vue
+++ src/components/areas/dashboard/widgets/_RatioWidget.vue
@@ -1,65 +1,39 @@
 <template>
   <q-card class="fit bordered q-pa-none" flat>
     <q-card-section class="q-pr-md q-pl-lg q-pt-lg q-pb-none">
-      <div class="row items-center justify-between">
-        <div class="justify-start">
-          <q-item class="no-padding">
-            <q-item-section avatar>
-              <slot name="avatar"></slot>
-            </q-item-section>
-            <q-item-section class="q-pl-xs">
-              <q-item-label class="text-h6 text-weight-700 q-mb-xs">
-                {{ title }}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </div>
-        <div class="justify-end">
+      <div>
+        <div class="float-right">
           <q-btn
             round
             unelevated
             dense
             :text-color="$q.dark.isActive ? 'white' : 'grey-8'"
-            icon="info"
-            @click="showInfo = !showInfo"
-          />
+            icon="o_info"
+          >
+            <q-tooltip
+              anchor="bottom right"
+              self="top middle"
+              style="width: 400px"
+              class="q-ma-md q-pa-md border-radius-lg custom-tooltip"
+            >
+              <slot name="info"></slot>
+            </q-tooltip>
+          </q-btn>
         </div>
-      </div>
-
-      <!-- <div class="row justify-between">
         <slot name="avatar"></slot>
-        <div class="row items-center justify-end">
-          <q-btn
-            unelevated
-            round
-            dense
-            :text-color="$q.dark.isActive ? 'white' : 'grey-8'"
-            size="md"
-            icon="info"
-            @click="showInfo = !showInfo"
-          />
-        </div>
-      </div> -->
+        <div class="q-mt-md text-h6 text-weight-700">{{ title }}</div>
+      </div>
     </q-card-section>
 
-    <q-card-section
-      v-if="!showInfo"
-      class="column q-gutter-y-sm q-px-lg q-py-lg"
-    >
+    <q-card-section class="q-px-lg q-pt-none">
       <div class="row items-center q-gutter-x-sm">
-        <div class="text-h2 text-weight-700 text-accent">
-          {{ ratioSign }}
-        </div>
-        <div
-          class="text-weight-700 text-secondary"
-          style="font-size: 42px"
-        >
+        <div class="text-h2 text-weight-700">
           {{ helper.formatNumber(ratio, fraction) }}
         </div>
+        <div class="text-h6 text-weight-700">
+          {{ ratioSign }}
+        </div>
       </div>
-    </q-card-section>
-    <q-card-section v-if="showInfo">
-      <slot name="info"></slot>
     </q-card-section>
   </q-card>
 </template>
