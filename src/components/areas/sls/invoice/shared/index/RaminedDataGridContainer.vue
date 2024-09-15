eb7d0bd9a2c3922a5ad95ecf9927de6014fcc79f
Mohammad Yengimolki <gammadesk@gmail.com>
2024-09-15 16:46:07 +0330

    remained invoice

---
+++ src/components/areas/sls/invoice/shared/index/RaminedDataGridContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <h4>مانده: {{ title }}</h4>
+  <data-grid-table
+    :title="title"
+    :table-store="tableStore"
+    :advanced-search="false"
+  />
+</template>
+
+<script setup>
+  import { useInvoiceFilteredGrid } from "components/areas/sls/_composables/useInvoiceFilteredGrid";
+  import { useDataTable } from "src/composables/useDataTable";
+
+  import DataGridTable from "./DataGridTable.vue";
+
+  const props = defineProps({
+    toolbar: Boolean,
+    title: String,
+    dataSource: { type: String, default: "sls/invoice/getGridData" },
+  });
+
+  const invoiceStore = useInvoiceFilteredGrid();
+  //const gridStore = useBaseInfoGrid(invoiceStore);
+  const tableStore = useDataTable({
+    dataSource: props.dataSource,
+    store: invoiceStore,
+  });
+</script>
