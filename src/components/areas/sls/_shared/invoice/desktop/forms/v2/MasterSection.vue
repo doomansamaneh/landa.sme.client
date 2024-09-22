a03bb695de8dc55067de4aac9abe7b6f8227519a
Khashayar Shomali <shomalikhashayar@gmail.com>
2024-09-22 19:25:57 +0330

    CreateV2 MasterSection.

--- src/components/areas/sls/_shared/invoice/desktop/forms/v2/MasterSection.vue
+++ src/components/areas/sls/_shared/invoice/desktop/forms/v2/MasterSection.vue
@@ -1,98 +1,147 @@
 <template>
-  <div class="row q-gutter-md">
-    <div style="width: 25%">
-      <div>
-        <q-item-label caption class="q-mb-sm">مشتری</q-item-label>
-        <customer-lookup
-          v-model:selectedId="model.value.customerId"
-          v-model:selectedText="model.value.customerName"
-        />
-      </div>
-    </div>
-    <div style="width: 25%">
-      <div>
-        <q-item-label caption class="q-mb-sm">بازایاب</q-item-label>
-        <customer-lookup
-          v-model:selectedId="model.value.contactId"
-          v-model:selectedText="model.value.contactName"
-        />
-      </div>
-    </div>
+  <div class="row">
+    <div class="col-md-8 col-sm-12 col-xs-12">
+      <div class="row q-col-gutter-md">
+        <div class="col-md-5 col-sm-12 col-xs-12">
+          <q-item-label caption class="q-mb-sm">
+            {{ customerTitle }}
+          </q-item-label>
+          <customer-lookup
+            v-model:selectedId="model.value.customerId"
+            v-model:selectedText="model.value.customerName"
+          />
+        </div>
 
-    <div style="width: 25%">
-      <div>
-        <q-item-label caption class="q-mb-sm">
-          شماره فاکتور
-        </q-item-label>
-        <q-input
-          type="number"
-          outlined
-          dense
-          v-model="invoiceNoField"
-          :disable="isInputDisabled"
-          style="width: 165px"
-        >
-          <template #append>
-            <q-icon
-              size="sm"
-              :name="invoiceNo ? 'o_no_encryption' : 'o_lock'"
-              class="cursor-pointer all-pointer-events"
-              @click="toggleInvocieNo"
-            />
-          </template>
-        </q-input>
+        <div class="col-md-5 col-sm-12 col-xs-12">
+          <q-item-label caption class="q-mb-sm">
+            بازاریاب
+          </q-item-label>
+          <customer-lookup
+            v-model:selectedId="model.value.contactId"
+            v-model:selectedText="model.value.contactName"
+          />
+        </div>
+
+        <div class="col row items-end">
+          <q-btn
+            v-if="$q.screen.gt.xs"
+            :text-color="$q.dark.isActive ? 'white' : 'grey-9'"
+            dense
+            round
+            icon="o_more_horiz"
+            unelevated
+            class="text-body2 no-letter-spacing"
+            @click="toggleMoreInfo"
+          />
+
+          <q-btn
+            v-if="$q.screen.xs"
+            round
+            size="11px"
+            unelevated
+            @click="toggleMoreInfo"
+          >
+            <q-icon size="20px" name="o_more_horiz" />
+          </q-btn>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-10 col-sm-12 col-xs-12">
+          <q-slide-transition>
+            <div class="q-mt-md" v-show="moreInfo">
+              <div class="row q-col-gutter-x-md">
+                <div class="col-md col-sm-12 col-xs-12">
+                  <q-item-label caption class="q-mb-sm">
+                    قرارداد
+                  </q-item-label>
+                  <contract-lookup
+                    v-model:selectedId="model.value.contractId"
+                    v-model:selectedText="model.value.contractTitle"
+                  />
+                </div>
+                <div class="col-md col-sm-12 col-xs-12">
+                  <q-item-label caption class="q-mb-sm">
+                    {{ saleTypeTitle }}
+                  </q-item-label>
+                  <sale-type-lookup
+                    v-model:selectedId="model.value.typeId"
+                    v-model:selectedText="model.value.typeTitle"
+                    :filter-expression="filterExpression"
+                  />
+                </div>
+              </div>
+            </div>
+          </q-slide-transition>
+
+          <div class="row q-mt-md">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <q-item-label caption class="q-mb-sm">شرح</q-item-label>
+              <custom-input
+                v-model="model.value.summary"
+                hide-bottom-space
+                type="textarea"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-
-    <div class="col row justify-end items-center">
-      <q-btn
-        unelevated
-        round
-        dense
-        icon="o_expand_more"
-        @click="showDetailSection"
-      />
+    <div class="col-md-4 col-sm-12 col-xs-12">
+      <div
+        class="row justify-end"
+        :class="{ 'q-mt-md': $q.screen.xs }"
+      >
+        <div class="col-md-6 col-sm-12 col-xs-12">
+          <q-item-label caption class="q-mb-sm">
+            شماره فاکتور
+          </q-item-label>
+          <q-input
+            type="number"
+            hide-bottom-space
+            v-model="model.value.no"
+            outlined
+            dense
+            :disable="!model.value.manualNo"
+          >
+            <template #append>
+              <q-icon
+                size="sm"
+                :name="invoiceNo ? 'o_no_encryption' : 'o_lock'"
+                class="cursor-pointer all-pointer-events"
+                @click="toggleInvocieNo"
+              />
+            </template>
+          </q-input>
+        </div>
+      </div>
+      <div v-if="showOriginalDoc" class="row justify-end q-mt-md">
+        <div class="col-md-6 col-sm-12 col-xs-12">
+          <q-item-label caption class="q-mb-sm">
+            سند مرجع
+          </q-item-label>
+          <pre>{{ model.value.originalDocument }}</pre>
+          <invoice-lookup
+            v-model:selectedId="model.value.originalDocument.parentId"
+            v-model:selectedText="model.value.originalDocument.no"
+            :filter-expression="originalFilterExpression"
+          />
+        </div>
+      </div>
+      <div class="row justify-end q-mt-md">
+        <div class="col-md-6 col-sm-12 col-xs-12">
+          <q-item-label caption class="q-mb-sm">تاریخ</q-item-label>
+          <date-time v-model="model.value.date" />
+        </div>
+      </div>
+      <div class="row justify-end q-mt-md">
+        <div class="col-md-6 col-sm-12 col-xs-12">
+          <q-item-label caption class="q-mb-sm">سررسید</q-item-label>
+          <date-time v-model="model.value.dueDate" />
+        </div>
+      </div>
     </div>
   </div>
-  <q-slide-transition>
-    <div v-show="detailSection">
-      <div class="row q-gutter-x-md q-mt-md">
-        <div style="width: 25%">
-          <div>
-            <q-item-label caption class="q-mb-sm">
-              قرارداد
-            </q-item-label>
-            <contract-lookup ref="contractLookup" />
-          </div>
-        </div>
-        <div style="width: 25%">
-          <div>
-            <q-item-label caption class="q-mb-sm">
-              نوع فروش
-            </q-item-label>
-            <sale-type-lookup />
-          </div>
-        </div>
-
-        <div class="col">
-          <div class="row q-gutter-md">
-            <div>
-              <q-item-label caption class="q-mb-sm">
-                تاریخ
-              </q-item-label>
-              <date-time v-model="date" style="width: 165px" />
-            </div>
-            <div>
-              <q-item-label caption class="q-mb-sm">
-                سررسید
-              </q-item-label>
-              <date-time v-model="dueDate" style="width: 165px" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </q-slide-transition>
 </template>
 
 <script setup>
@@ -105,8 +154,16 @@
   import CustomInput from "src/components/shared/forms/CustomInput.vue";
   import CustomSelect from "src/components/shared/forms/CustomSelect.vue";
 
+  import {
+    sqlOperator,
+    vatType,
+    invoiceFormType,
+    documentType,
+  } from "src/constants";
+
   const props = defineProps({
     formStore: Object,
+    formType: Object,
   });
 
   const contractLookup = ref(null);
@@ -116,6 +173,39 @@
   const dueDate = ref("");
   const customerLookup = ref(null);
   const detailSection = ref(false);
+
+  const moreInfo = ref(false);
+
+  const customerTitle =
+    props.formType == invoiceFormType.sales ? "مشتری" : "فروشنده";
+
+  const saleTypeTitle =
+    props.formType == invoiceFormType.sales ? "نوع فروش" : "نوع خرید";
+
+  const originalFilterExpression = [
+    {
+      fieldName: "d.typeId",
+      operator: sqlOperator.in,
+      value: `${documentType.invoice},${documentType.salesReturn}`,
+    },
+  ];
+
+  const filterExpression =
+    props.formType == invoiceFormType.sales
+      ? [
+          {
+            fieldName: "isForSale",
+            operator: sqlOperator.equal,
+            value: vatType.sale,
+          },
+        ]
+      : [
+          {
+            fieldName: "isForSale",
+            operator: sqlOperator.equal,
+            value: vatType.purchase,
+          },
+        ];
 
   const toggleInvocieNo = () => {
     localFormStore.value.model.value.manualNo =
@@ -131,10 +221,9 @@
       (props.formType === invoiceFormType.sales ||
         props.formType === invoiceFormType.salesReturn)
   );
-  const isInputDisabled = computed(() => !invoiceNo.value);
 
-  const showDetailSection = () => {
-    detailSection.value = !detailSection.value;
+  const toggleMoreInfo = () => {
+    moreInfo.value = !moreInfo.value;
   };
 </script>
 
