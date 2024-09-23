4a2e4a7e19fd96fd68a6a0eea1b8817fd2ab8e3c
Khashayar Shomali <shomalikhashayar@gmail.com>
2024-09-23 22:34:25 +0330

    bankLogos.

--- src/components/areas/dashboard/widgets/_CashBalanceList.vue
+++ src/components/areas/dashboard/widgets/_CashBalanceList.vue
@@ -5,28 +5,37 @@
     class="q-pr-sm q-pl-none q-pb-md border-radius-xs text-on-dark"
   >
     <div class="row q-gutter-x-sm items-center">
-      <q-avatar
-        class="primary-gradient primary-shadow border-radius-xs"
-        square
-        v-if="item.picture"
-        text-color="white"
-        size="58px"
+      <q-btn
+        v-if="getBankLogo(item.label)"
+        class="bordered no-pointer-events border-radius-xs"
+        unelevated
+        dense
+        padding="10px"
       >
-        <img :src="item.picture" />
-      </q-avatar>
-
-      <q-avatar
-        class="border-radius-xs"
-        square
-        size="58px"
-        text-color="white"
-        :style="helper.generateAvatarStyle(item.id)"
+        <img
+          :src="getBankLogo(item.label)"
+          alt="Bank Logo"
+          style="width: 42px; height: 42px"
+        />
+      </q-btn>
+      <q-btn
         v-else
+        class="bordered no-pointer-events border-radius-xs"
+        unelevated
+        dense
+        padding="10px"
       >
-        <div class="char text-body1 text-bold">
-          {{ helper.getFirstChar(item.label) }}
+        <div
+          class="flex items-center justify-center"
+          style="width: 42px; height: 42px"
+        >
+          <img
+            src="/bank.svg"
+            alt="Bank Logo"
+            style="width: 36px; height: 36px"
+          />
         </div>
-      </q-avatar>
+      </q-btn>
     </div>
 
     <div class="row items-center justify-between full-width">
@@ -51,10 +60,29 @@
 <script setup>
   import { helper } from "src/helpers";
   import { useBankAccount } from "../../acc/_composables/useBankAccount";
-
+  import { getBankNameFromCardNumber } from "@persian-tools/persian-tools"; // Import Persian Tools method
   import GotoDetail from "src/components/shared/buttons/GotoDetail.vue";
 
   const props = defineProps({
     dataSource: useBankAccount,
   });
+
+  function getBankName(label) {
+    const cardNumber = label.match(/\d{16}/);
+
+    if (!cardNumber) {
+      return null;
+    }
+
+    const bankName = getBankNameFromCardNumber(cardNumber[0]);
+
+    return bankName;
+  }
+
+  function getBankLogo(label) {
+    const bankName = getBankName(label);
+    if (bankName && helper.bankLogos[bankName]) {
+      return helper.bankLogos[bankName];
+    }
+  }
 </script>
