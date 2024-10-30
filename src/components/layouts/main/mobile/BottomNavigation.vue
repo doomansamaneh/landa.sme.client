3229640fd058d0c31c1b5ae0ff8d4985fc4bc2d5
Khashayar Shomali <shomalikhashayar@gmail.com>
2024-10-30 20:03:14 +0330

    BottomNavigation(NavigationItem)

--- src/components/layouts/main/mobile/BottomNavigation.vue
+++ src/components/layouts/main/mobile/BottomNavigation.vue
@@ -2,124 +2,34 @@
   <q-footer
     v-if="$q.screen.lt.sm"
     bordered
-    class="bottom-navigation text-on-dark"
+    class="bottom-navigation-background text-on-dark"
     :class="$q.dark.isActive ? 'bg-dark' : 'bg-light'"
   >
     <div class="row items-center q-py-sm">
-      <div class="col text-center">
-        <div @click="menuBarStore.toggle()">
-          <q-btn
-            dense
-            unelevated
-            padding="2px 14px"
-            rounded
-            :class="
-              menuBarStore.state.visible.value
-                ? 'primary-gradient active-shine text-white'
-                : ''
-            "
-          >
-            <q-icon
-              size="18px"
-              :name="
-                menuBarStore.state.visible.value ? 'menu' : 'o_menu'
-              "
-            />
-          </q-btn>
-          <div
-            class="q-mt-xs text-center text-caption text-weight-500 no-letter-spacing"
-          >
-            منو
-          </div>
-        </div>
-      </div>
-
-      <div class="col text-center">
-        <div @click="goToDashboard">
-          <q-btn
-            dense
-            unelevated
-            padding="2px 14px"
-            rounded
-            :class="
-              isActiveDashboard
-                ? 'primary-gradient active-shine text-white'
-                : ''
-            "
-          >
-            <q-icon
-              size="18px"
-              :name="isActiveDashboard ? 'dashboard' : 'o_dashboard'"
-            />
-          </q-btn>
-          <div
-            class="q-mt-xs text-center text-caption text-weight-500 no-letter-spacing"
-          >
-            پیشخوان
-          </div>
-        </div>
-      </div>
-
-      <div class="col text-center">
-        <div @click="contactDrawerStore.toggle()">
-          <q-btn
-            dense
-            unelevated
-            padding="2px 14px"
-            rounded
-            :class="
-              contactDrawerStore.state.value
-                ? 'primary-gradient active-shine text-white'
-                : ''
-            "
-          >
-            <q-icon
-              size="18px"
-              :name="
-                contactDrawerStore.state.value
-                  ? 'person_search'
-                  : 'o_person_search'
-              "
-            />
-          </q-btn>
-          <div
-            class="q-mt-xs text-center text-caption text-weight-500 no-letter-spacing"
-          >
-            مخاطبین
-          </div>
-        </div>
-      </div>
-
-      <div class="col text-center">
-        <div @click="goToProfile">
-          <q-btn
-            dense
-            unelevated
-            padding="2px 14px"
-            rounded
-            class="no-pointer-events"
-            :class="
-              isActiveProfile
-                ? 'primary-gradient active-shine text-white'
-                : ''
-            "
-          >
-            <q-icon
-              size="18px"
-              :name="
-                isActiveProfile
-                  ? 'account_circle'
-                  : 'o_account_circle'
-              "
-            />
-          </q-btn>
-          <div
-            class="q-mt-xs text-center text-caption text-weight-500 no-letter-spacing"
-          >
-            نمایه
-          </div>
-        </div>
-      </div>
+      <navigation-item
+        label="منو"
+        icon="menu"
+        :isActive="menuBarStore.state.visible.value"
+        @click="menuBarStore.toggle"
+      />
+      <navigation-item
+        label="پیشخوان"
+        icon="dashboard"
+        :isActive="isActiveDashboard"
+        @click="goToDashboard"
+      />
+      <navigation-item
+        label="مخاطبین"
+        icon="person_search"
+        :isActive="contactDrawerStore.state.value"
+        @click="contactDrawerStore.toggle"
+      />
+      <navigation-item
+        label="نمایه"
+        icon="account_circle"
+        :isActive="isActiveProfile"
+        @click="goToProfile"
+      />
     </div>
   </q-footer>
 </template>
@@ -127,9 +37,10 @@
 <script setup>
   import { computed } from "vue";
   import { useRouter, useRoute } from "vue-router";
-
   import { useContactDrawer } from "src/composables/useContactDrawer";
   import { useMenuBar } from "src/composables/useMenuBar";
+
+  import NavigationItem from "src/components/layouts/main/mobile/NavigationItem.vue";
 
   const router = useRouter();
   const route = useRoute();
@@ -137,13 +48,8 @@
   const contactDrawerStore = useContactDrawer();
   const menuBarStore = useMenuBar();
 
-  const goToDashboard = () => {
-    router.push("/dashboard");
-  };
-
-  const goToProfile = () => {
-    router.push("/scr/users/settings");
-  };
+  const goToDashboard = () => router.push("/dashboard");
+  const goToProfile = () => router.push("/scr/users/settings");
 
   const isActiveDashboard = computed(
     () => route.path === "/dashboard"
---
+++ src/components/layouts/main/mobile/NavigationItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="col text-center" @click="handleClick">
+    <q-btn
+      dense
+      unelevated
+      padding="2px 14px"
+      rounded
+      :class="buttonClass"
+    >
+      <q-icon :size="iconSize" :name="iconFill" />
+    </q-btn>
+    <div
+      :class="labelClass"
+      class="q-mt-xs text-center text-caption text-weight-500 no-letter-spacing"
+    >
+      {{ label }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { computed, defineProps, defineEmits } from "vue";
+  import { useQuasar } from "quasar";
+
+  const props = defineProps({
+    label: String,
+    icon: String,
+    isActive: Boolean,
+  });
+
+  const emits = defineEmits(["click"]);
+
+  const $q = useQuasar();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    emits("click");
+  };
+
+  const buttonClass = computed(() =>
+    props.isActive
+      ? $q.dark.isActive
+        ? "bg-yellow-7 active-shine text-dark"
+        : "primary-gradient active-shine text-white"
+      : ""
+  );
+
+  const labelClass = computed(() =>
+    props.isActive
+      ? $q.dark.isActive
+        ? "text-yellow"
+        : "text-primary"
+      : ""
+  );
+
+  const iconFill = computed(() =>
+    props.isActive ? props.icon : `o_${props.icon}`
+  );
+
+  const iconSize = "18px";
+</script>
