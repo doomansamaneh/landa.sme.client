2fb9c39029d45bf46eaf12e35988e3f5735c80ba
Khashayar Shomali <shomalikhashayar@gmail.com>
2025-01-08 10:24:33 +0330

    Business DataGrid.

--- src/components/shared/Lookups/PlanLookup.vue
+++ src/components/shared/Lookups/PlanLookup.vue
@@ -5,21 +5,33 @@
     sortColumn="title"
     textTemplate="{{ title }}"
     searchField="title"
-    columns="code,title"
+    columns="title,cost"
     :placeholder="$t('page.add-business.plan-lookup.placeholder')"
-    :required="true"
+    required
   >
-    <template #td="{ row, index }">
-      <div class="row items-center q-col-gutter-md_ q-pa-md">
-        <div style="width: 20px" class="text-body3 no-letter-spacing">
-          {{ index }}
-        </div>
-        <div class="col-8 text-body2 no-letter-spacing">
-          {{ row.title }}
-        </div>
-        <div class="col text-body2 no-letter-spacing">
-          {{ row.cost.toLocaleString() }}
-        </div>
+    <template #thead-index>
+      <div>#</div>
+    </template>
+
+    <template #thead-cols>
+      <div class="col">عنوان</div>
+      <div class="col-1">هزینه</div>
+    </template>
+
+    <template #tbody-index="{ index }">
+      <div>{{ index + 1 }}</div>
+    </template>
+
+    <template #tbody-cols="{ item }">
+      <div class="col">
+        <span class="q-mr-sm text-body2 no-letter-spacing">
+          {{ item.title }}
+        </span>
+      </div>
+      <div class="col-1">
+        <span class="text-body2 no-letter-spacing">
+          {{ item.cost.toLocaleString() }}
+        </span>
       </div>
     </template>
   </lookup-view>
