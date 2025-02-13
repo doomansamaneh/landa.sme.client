e8b66ab3d5d79175b2820a49ddc930058ead3568
Khashayar Shomali <shomalikhashayar@gmail.com>
2025-02-13 16:03:04 +0330

    Prl/Wage print preview.

--- src/components/areas/prl/wage/desktop/preview/printPreview/BodySection.vue
+++ src/components/areas/prl/wage/desktop/preview/printPreview/BodySection.vue
@@ -1,37 +1,111 @@
 <template>
-  <table
-    class="print-preview-table"
-    v-if="!tableStore.showLoader.value"
-  >
-    <thead>
-      <tr>
-        <th style="width: 1px">#</th>
-        <th
-          v-for="column in tableStore.columns.value"
-          :key="column.field"
-        >
-          {{ column.label }}
-        </th>
-      </tr>
-    </thead>
+  <table class="print-preview-table">
+    <tr>
+      <td rowspan="2" style="width: 5px">#</td>
+      <td>پرسنل</td>
+      <td>شروع کار</td>
+      <td>کارکرد</td>
+      <td>دستمزد روزانه</td>
+      <td>پایه سنوات</td>
+      <td>حق مسکن</td>
+      <td>بن</td>
+      <td>حق اولاد + حق تعهل</td>
+      <td>ماموریت</td>
+      <td>بیمه کارگر</td>
+      <td>جمع کل</td>
+    </tr>
+    <tr>
+      <td>ش بیمه</td>
+      <td>ترک کار</td>
+      <td>اضافه کار</td>
+      <td>پاداش بهره‌وری</td>
+      <td>عیدی و پاداش</td>
+      <td>بازخرید سنوات</td>
+      <td>بازخرید مرخصی</td>
+      <td>سایر مزایا</td>
+      <td>سایر کسورات</td>
+      <td>مالیات</td>
+      <td>قابل پرداخت</td>
+    </tr>
     <tbody>
-      <tr
+      <template
         v-for="(row, rowIndex) in tableStore.rows.value"
         :key="rowIndex"
       >
-        <td style="width: 1px">{{ rowIndex + 1 }}</td>
+        <tr>
+          <td rowspan="2" style="width: 1px">{{ rowIndex + 1 }}</td>
+          <td>{{ row.customerCode }} - {{ row.customerName }}</td>
+          <td>{{ row.dateEnter }}</td>
+          <td>{{ row.day }}</td>
+          <td>{{ helper.formatNumber(row.salary) }}</td>
+          <td>{{ helper.formatNumber(row.baseYear) }}</td>
+          <td>{{ helper.formatNumber(row.housing) }}</td>
+          <td>{{ helper.formatNumber(row.food) }}</td>
+          <td>{{ helper.formatNumber(row.child + row.spouse) }}</td>
+          <td>{{ helper.formatNumber(row.mission) }}</td>
+          <td>{{ helper.formatNumber(row.insurranceEmp) }}</td>
+          <td>{{ helper.formatNumber(row.amount) }}</td>
+        </tr>
 
-        <td
-          v-for="column in tableStore.columns.value"
-          :key="column.field"
-        >
-          {{ formatValue(row[column.field], column.type) }}
-        </td>
-      </tr>
+        <tr>
+          <td>{{ row.insurranceNo }}</td>
+          <td>{{ row.dateExit }}</td>
+          <td>{{ helper.formatNumber(row.overtime) }}</td>
+          <td>{{ helper.formatNumber(row.reward) }}</td>
+          <td>{{ helper.formatNumber(row.bonus) }}</td>
+          <td>{{ helper.formatNumber(row.serviceBenefit) }}</td>
+          <td>{{ row.leave }}</td>
+          <td>{{ helper.formatNumber(row.addition) }}</td>
+          <td>{{ helper.formatNumber(row.deduction) }}</td>
+          <td>{{ helper.formatNumber(row.taxAmount) }}</td>
+          <td>{{ helper.formatNumber(row.payable) }}</td>
+        </tr>
+      </template>
     </tbody>
+
     <tfoot>
       <tr>
-        <td colspan="100%">جمع کل</td>
+        <td colspan="10" rowspan="2" style="text-align: left">
+          <strong>جمع کل</strong>
+        </td>
+        <td>
+          <b>
+            {{
+              helper.formatNumber(
+                tableStore?.summaryData?.value?.amount
+              )
+            }}
+          </b>
+        </td>
+        <td>
+          <b>
+            {{
+              helper.formatNumber(
+                tableStore?.summaryData?.value?.taxAmount
+              )
+            }}
+          </b>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <b>
+            {{
+              helper.formatNumber(
+                tableStore?.summaryData?.value?.amount
+              )
+            }}
+          </b>
+        </td>
+        <td>
+          <b>
+            {{
+              helper.formatNumber(
+                tableStore?.summaryData?.value?.taxAmount
+              )
+            }}
+          </b>
+        </td>
       </tr>
     </tfoot>
   </table>
@@ -44,7 +118,6 @@
   import { onMounted } from "vue";
   import { useDataTable } from "src/composables/useDataTable";
   import { helper } from "src/helpers";
-  import { dataType } from "src/constants";
 
   const props = defineProps({
     wageId: String,
@@ -66,13 +139,6 @@
     dataSource: "prl/wageItem/getGridData",
     store: gridStore,
   });
-
-  const formatValue = (value, type) => {
-    if (type === dataType.date) return value.substring(0, 10);
-    if (type === dataType.number) return helper.formatNumber(value);
-
-    return value;
-  };
 
   onMounted(() => {
     tableStore.loadData();
