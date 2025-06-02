3c40cef93a88354de3aca37e7bbc5b6af4557171
Khashayar Shomali <shomalikhashayar@gmail.com>
2025-06-02 17:26:21 +0330

    Clean scr/settings

---
+++ src/components/areas/scr/users/settings/SettingsCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-card class="bordered overflow-hidden">
+    <q-card-section class="no-padding">
+      <slot></slot>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+</script>
--- src/pages/management/users/SettingPage.vue
+++ src/pages/management/users/SettingPage.vue
@@ -3,5 +3,5 @@
 </template>
 
 <script setup>
-  import Desktop from "src/components/areas/scr/users/UserSetting.vue";
+  import Desktop from "src/components/areas/scr/users/settings/IndexView.vue"
 </script>
