58f3ab0d22fdb767dd6a6d5af523d778e5b1d95a
yengimolki <gammadesk@gmail.com>
2025-07-23 22:49:48 +0330

    no-product-found

--- src/components/areas/sls/_shared/invoice/desktop/forms/NoProductSelected.vue
+++ src/components/areas/sls/_shared/invoice/desktop/forms/NoProductSelected.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="column items-center q-gutter-md q-py-md">
     <img src="/productshelf.svg" style="width: 120px" alt="" />
-    <div class="text-body1">{{ $t("shared.no-product-found") }}</div>
+    <div class="text-body1">
+      {{ $t("shared.labels.no-product-found") }}
+    </div>
   </div>
 </template>
 
--- src/components/areas/sls/_shared/invoice/mobile/forms/NoProductSelected.vue
+++ src/components/areas/sls/_shared/invoice/mobile/forms/NoProductSelected.vue
@@ -2,7 +2,7 @@
   <div class="column items-center q-gutter-md q-py-md">
     <img src="/productshelf.svg" style="width: 120px" alt="" />
     <div class="text-body1">
-      {{ $t("shared.no-product-found") }}
+      {{ $t("shared.labels.no-product-found") }}
     </div>
   </div>
 </template>
