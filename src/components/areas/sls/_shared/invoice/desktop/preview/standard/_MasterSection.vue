b3b9d365e6e322ffb28ebf195962ae0199be5a6c
Mohammad Yengimolki <gammadesk@gmail.com>
2025-09-08 18:56:32 +0330

    invoice logo, signature

--- src/components/areas/sls/_shared/invoice/desktop/preview/standard/_MasterSection.vue
+++ src/components/areas/sls/_shared/invoice/desktop/preview/standard/_MasterSection.vue
@@ -2,33 +2,41 @@
   <q-card bordered>
     <div :ref="printStore.printRef">
       <q-card-section>
-        <invoice-header show-logo :model="model" :title="title" />
+        <invoice-header
+          :show-logo="showSaleHeader"
+          :model="model"
+          :title="title"
+        />
       </q-card-section>
       <q-card-section class="q-gutter-y-sm">
         <header-sale v-if="showSaleHeader" :model="model" />
         <header-purchase v-else :model="model" />
         <invoice-body :model="model" />
-        <invoice-footer show-signature :model="model" :comment="comment" />
+        <invoice-footer
+          :show-signature="showSaleHeader"
+          :model="model"
+          :comment="comment"
+        />
       </q-card-section>
     </div>
   </q-card>
 </template>
 
 <script setup>
-import { usePrint } from "src/composables/usePrint";
+  import { usePrint } from "src/composables/usePrint";
 
-import InvoiceHeader from "./_HeaderSection.vue";
-import HeaderSale from "./_HeaderSale.vue";
-import HeaderPurchase from "./_HeaderPurchase.vue";
-import InvoiceBody from "./_BodySection.vue";
-import InvoiceFooter from "./_FooterSection.vue";
+  import InvoiceHeader from "./_HeaderSection.vue";
+  import HeaderSale from "./_HeaderSale.vue";
+  import HeaderPurchase from "./_HeaderPurchase.vue";
+  import InvoiceBody from "./_BodySection.vue";
+  import InvoiceFooter from "./_FooterSection.vue";
 
-const props = defineProps({
-  model: Object,
-  title: String,
-  showSaleHeader: Boolean,
-  comment: String,
-});
+  const props = defineProps({
+    model: Object,
+    title: String,
+    showSaleHeader: Boolean,
+    comment: String,
+  });
 
-const printStore = usePrint();
+  const printStore = usePrint();
 </script>
