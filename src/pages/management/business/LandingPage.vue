b84d6fbc9019bfb3738234427dbf638ba9c41159
Mohammad Yengimolki <gammadesk@gmail.com>
2025-09-19 15:03:09 +0330

    invoice api tax

--- src/pages/management/business/LandingPage.vue
+++ src/pages/management/business/LandingPage.vue
@@ -10,21 +10,10 @@
       <background v-if="$q.screen.gt.xs" />
       <index-view />
     </div>
-
-    <!-- Show CongratsDialog if firstLogin is true -->
-    <!-- <congrats-dialog v-if="showCongratsDialog" /> -->
   </q-page>
 </template>
 
 <script setup>
-  // import { computed } from "vue";
-  import { useFirstLogin } from "src/composables/useFirstLogin";
   import Background from "src/assets/BusinessBackground.vue";
   import IndexView from "src/components/management/business/FirstLoginWizard.vue";
-  // import CongratsDialog from "src/components/shared/CongratsDialog.vue";
-
-  const tutorialStore = useFirstLogin();
-  // const showCongratsDialog = computed(
-  //   () => tutorialStore.firstLogin.value
-  // );
 </script>
