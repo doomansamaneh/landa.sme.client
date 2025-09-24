5386dc9fdfd15ff5019e67e53a05d0d0a266c812
Khashayar Shomali <shomalikhashayar@gmail.com>
2025-09-24 20:19:21 +0330

    ConnectionLostDialog.

--- src/components/shared/ConnectionLostDialog.vue
+++ src/components/shared/ConnectionLostDialog.vue
@@ -8,7 +8,7 @@
     style="z-index: 9999 !important"
   >
     <q-card class="column items-center justify-center">
-      <q-card-section class="q-mb-xl">
+      <q-card-section class="q-mb-xl q-pb-xl">
         <div class="text-h6 text-center full-width">
           <q-icon
             name="o_wifi_off"
@@ -17,10 +17,10 @@
             class="q-mb-sm"
           />
 
-          <div class="text-body1 text-weight-700">
+          <div class="text-body1 caption-on-dark q-mb-xs">
             {{ $t("messages.connectionLost") }}
           </div>
-          <div class="text-body1 text-weight-700 caption-on-dark">
+          <div class="text-body text-weight-700 caption-on-dark">
             {{ $t("messages.checkConnection") }}
           </div>
         </div>
@@ -32,9 +32,15 @@
             padding="4px 16px"
             color="primary"
             rounded
+            :disable="isLoading"
             @click="retryConnection"
           >
-            {{ $t("shared.labels.retry") }}
+            <span v-if="isLoading">
+              {{ $t("messages.checkingConnection") }}
+            </span>
+            <span v-else>
+              {{ $t("shared.labels.retry") }}
+            </span>
           </q-btn>
         </div>
       </q-card-section>
@@ -61,7 +67,39 @@
 </template>
 
 <script setup>
+  import { ref } from "vue";
+  import {
+    fetchWrapper,
+    isConnectionDialogOpen,
+  } from "src/helpers/fetch-wrapper";
+  import { useQuasar } from "quasar";
+  import { useComposables } from "src/stores/useComposables";
+  import { bus } from "src/helpers";
+
+  const $q = useQuasar();
+  const isLoading = ref(false);
+  const dialogRef = ref(null);
+  const composablesStore = useComposables();
+
+  const reloadData = () => {
+    composablesStore.reset();
+    bus.emit("render-page");
+  };
+
   const retryConnection = async () => {
-    window.location.reload();
+    try {
+      isLoading.value = true;
+      const success = await fetchWrapper.retryConnection();
+
+      if (success) {
+        reloadData();
+        isConnectionDialogOpen.value = false;
+        dialogRef.value.hide();
+      }
+    } catch (error) {
+      // Connection still failed, keep dialog open
+    } finally {
+      isLoading.value = false;
+    }
   };
 </script>
